1fb3abe8f7096bed70126cd86228b27d42a838be
Chris Friedt <cfriedt@tenstorrent.com>
2025-01-22 11:32:28 -0500

    lib: tenstorrent: bh_chip: add copyright
    Add missing copyright to bh_arc.c.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- lib/tenstorrent/bh_chip/bh_arc.c
+++ lib/tenstorrent/bh_chip/bh_arc.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Tenstorrent AI ULC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include <tenstorrent/bh_arc.h>
 
 int bharc_smbus_block_read(const struct bh_arc *dev, uint8_t cmd, uint8_t *count, uint8_t *output)
