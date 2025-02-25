3f661a4ee881abd3bafd3863388d56db8e2d8965
Chris Friedt <cfriedt@tenstorrent.com>
2025-02-25 12:32:04 -0500

    enable vuart
    Enable virtual uart.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- drivers/serial/uart_tt_virt.c
+++ drivers/serial/uart_tt_virt.c
@@ -6,11 +6,11 @@
 
 #define DT_DRV_COMPAT tenstorrent_uart_virt
 
-#include "status_reg.h"
-
 #include <errno.h>
+#include <stdatomic.h>
 
 #include <tenstorrent/uart_tt_virt.h>
+#include <zephyr/device.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -18,90 +18,41 @@
 
 LOG_MODULE_REGISTER(uart_tt_virt, CONFIG_UART_LOG_LEVEL);
 
-#define UART_TT_VIRT_DISCOVERY_ADDR RESET_UNIT_SCRATCH_RAM_REG_ADDR(42)
+#ifdef CONFIG_BOARD_TT_BLACKHOLE_TT_BLACKHOLE_SMC
+#include "status_reg.h"
+
+#undef UART_TT_VIRT_DISCOVERY_ADDR
+#define UART_TT_VIRT_DISCOVERY_ADDR ((uintptr_t *)RESET_UNIT_SCRATCH_RAM_REG_ADDR(42))
+#else
+#undef UART_TT_VIRT_DISCOVERY_ADDR
+#define UART_TT_VIRT_DISCOVERY_ADDR (&uart_tt_virt_discovery)
+static uintptr_t uart_tt_virt_discovery;
+#endif
 
 struct uart_tt_virt_config {
-	struct uart_tt_virt_desc *desc;
-	uint32_t tx_buf_capacity;
-	uint32_t rx_buf_capacity;
+	volatile struct tt_vuart *vuart;
 };
 
 struct uart_tt_virt_data {
-	struct k_spinlock rx_lock;
-	struct k_spinlock tx_lock;
+#ifdef CONFIG_UART_USE_RUNTIME_CONFIGURE
+	struct uart_config cfg;
+#endif
 };
 
 static int uart_tt_virt_poll_in(const struct device *dev, unsigned char *p_char)
 {
-	int ret = 0;
-	struct uart_tt_virt_data *data = dev->data;
 	const struct uart_tt_virt_config *config = dev->config;
-	struct uart_tt_virt_desc *desc = config->desc;
+	volatile struct tt_vuart *vuart = config->vuart;
 
-	K_SPINLOCK(&data->rx_lock) {
-		uint32_t head = sys_read32((mem_addr_t)&desc->rx_head);
-		uint32_t tail = sys_read32((mem_addr_t)&desc->rx_tail);
-
-		if (head == tail) {
-			/* if up-counters are equal, buffer is empty */
-			ret = -1;
-			K_SPINLOCK_BREAK;
-		}
-
-		*p_char = sys_read8((mem_addr_t)(&desc->buf[config->tx_buf_capacity +
-							    (head % config->rx_buf_capacity)]));
-		sys_write32(head + 1, (mem_addr_t)&desc->rx_head);
-	}
-
-	return ret;
+	return tt_vuart_poll_in(vuart, p_char, TT_VUART_ROLE_DEVICE);
 }
 
-static void uart_tt_virt_poll_out(const struct device *dev, unsigned char out_char)
+void uart_tt_virt_poll_out(const struct device *dev, unsigned char out_char)
 {
-	struct uart_tt_virt_data *data = dev->data;
 	const struct uart_tt_virt_config *config = dev->config;
-	struct uart_tt_virt_desc *const desc = config->desc;
+	volatile struct tt_vuart *const vuart = config->vuart;
 
-	K_SPINLOCK(&data->tx_lock) {
-		uint32_t head = sys_read32((mem_addr_t)&desc->tx_head);
-		uint32_t tail = sys_read32((mem_addr_t)&desc->tx_tail) + 1;
-
-		if (tail >= head + config->tx_buf_capacity) {
-			/*
-			 * Normally, for physical uarts, a full TX buffer is not an issue; if
-			 * transmit is enabled, then it is usually only a few microseconds until
-			 * space becomes available. However, with this virtual uart, we rely on a
-			 * host-side process to empty the buffer, which could mean intolerably long
-			 * delays.
-			 *
-			 * The behaviour in this case is controlled by
-			 * CONFIG_UART_TT_VIRT_OFLOW_CHOICE.
-			 */
-			if (IS_ENABLED(CONFIG_UART_TT_VIRT_OFLOW_HEAD)) {
-				/*
-				 * Note: this is inherently racey since the host updates the head
-				 * counter
-				 */
-				sys_write8(out_char,
-					   (mem_addr_t)&desc->buf[head % config->tx_buf_capacity]);
-			} else if (IS_ENABLED(CONFIG_UART_TT_VIRT_OFLOW_TAIL)) {
-				sys_write8(out_char,
-					   (mem_addr_t)&desc
-						   ->buf[(tail - 1) % config->tx_buf_capacity]);
-			} else if (IS_ENABLED(CONFIG_UART_TT_VIRT_OFLOW_DROP)) {
-				/* do nothing - character is lost */
-			}
-
-			/* increment overflow counter */
-			sys_write32(sys_read32((mem_addr_t)&desc->tx_oflow) + 1,
-				    (mem_addr_t)&desc->tx_oflow);
-
-			K_SPINLOCK_BREAK;
-		}
-
-		sys_write8(out_char, (mem_addr_t)&desc->buf[tail % config->tx_buf_capacity]);
-		sys_write32(tail, (mem_addr_t)&desc->tx_tail);
-	}
+	tt_vuart_poll_out(vuart, out_char, TT_VUART_ROLE_DEVICE);
 }
 
 static int uart_tt_virt_err_check(const struct device *dev)
@@ -111,22 +62,48 @@ static int uart_tt_virt_err_check(const struct device *dev)
 	return 0;
 }
 
-static const struct uart_driver_api uart_tt_virt_api = {
+#ifdef CONFIG_UART_USE_RUNTIME_CONFIGURE
+static int uart_tt_virt_configure(const struct device *dev, const struct uart_config *cfg)
+{
+	struct uart_tt_virt_data *data = dev->data;
+
+	memcpy(&data->cfg, cfg, sizeof(*cfg));
+	return 0;
+}
+
+static int uart_tt_virt_config_get(const struct device *dev, struct uart_config *cfg)
+{
+	struct uart_tt_virt_data *data = dev->data;
+
+	memcpy(cfg, &data->cfg, sizeof(*cfg));
+	return 0;
+}
+#endif /* CONFIG_UART_USE_RUNTIME_CONFIGURE */
+
+static DEVICE_API(uart, uart_tt_virt_api) = {
 	.poll_in = uart_tt_virt_poll_in,
 	.poll_out = uart_tt_virt_poll_out,
+#ifdef CONFIG_UART_USE_RUNTIME_CONFIGURE
+	.configure = uart_tt_virt_configure,
+	.config_get = uart_tt_virt_config_get,
+#endif
 	.err_check = uart_tt_virt_err_check,
 };
+
+volatile struct tt_vuart *uart_tt_virt_get(const struct device *dev)
+{
+	const struct uart_tt_virt_config *config = dev->config;
+
+	return config->vuart;
+}
 
 static int uart_tt_virt_init(const struct device *dev)
 {
 	const struct uart_tt_virt_config *config = dev->config;
-	struct uart_tt_virt_desc *desc = config->desc;
+	volatile struct tt_vuart *vuart = config->vuart;
 
-	sys_write32(config->tx_buf_capacity, (mem_addr_t)&desc->tx_buf_capacity);
-	sys_write32(config->rx_buf_capacity, (mem_addr_t)&desc->rx_buf_capacity);
-	sys_write32(UART_TT_VIRT_MAGIC, (mem_addr_t)&desc->magic);
-
-	sys_write32((uint32_t)&desc, (mem_addr_t)UART_TT_VIRT_DISCOVERY_ADDR);
+	vuart->magic = UART_TT_VIRT_MAGIC;
+	*UART_TT_VIRT_DISCOVERY_ADDR = (uintptr_t)vuart;
 
 	return 0;
 }
@@ -135,16 +112,14 @@ static int uart_tt_virt_init(const struct device *dev)
 #define UART_TT_VIRT_RX_BUF_SIZE(_inst) DT_INST_PROP(_inst, rx_buf_size)
 
 #define UART_TT_VIRT_DESC_SIZE(_inst)                                                              \
-	(DIV_ROUND_UP(sizeof(struct uart_tt_virt_desc) + UART_TT_VIRT_TX_BUF_SIZE(_inst) +         \
+	(DIV_ROUND_UP(sizeof(struct tt_vuart) + UART_TT_VIRT_TX_BUF_SIZE(_inst) +                  \
 			      UART_TT_VIRT_RX_BUF_SIZE(_inst),                                     \
 		      sizeof(uint32_t)))
 
 #define DEFINE_UART_TT_VIRT(_inst)                                                                 \
-	static uint32_t uart_tt_virt_desc_##_inst[UART_TT_VIRT_DESC_SIZE(_inst)];                  \
+	static uint32_t tt_vuart_##_inst[UART_TT_VIRT_DESC_SIZE(_inst)];                           \
 	static const struct uart_tt_virt_config uart_tt_virt_config_##_inst = {                    \
-		.desc = (struct uart_tt_virt_desc *)&uart_tt_virt_desc_##_inst,                    \
-		.tx_buf_capacity = UART_TT_VIRT_TX_BUF_SIZE(_inst),                                \
-		.rx_buf_capacity = UART_TT_VIRT_RX_BUF_SIZE(_inst),                                \
+		.vuart = (struct tt_vuart *)&tt_vuart_##_inst,                                     \
 	};                                                                                         \
 	static struct uart_tt_virt_data uart_tt_virt_data_##_inst;                                 \
                                                                                                    \
--- include/tenstorrent/uart_tt_virt.h
+++ include/tenstorrent/uart_tt_virt.h
@@ -7,16 +7,30 @@
 #ifndef TENSTORRENT_UART_TT_VIRT_H_
 #define TENSTORRENT_UART_TT_VIRT_H_
 
+#include <stdatomic.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#ifndef RESET_UNIT_SCRATCH_RAM_BASE_ADDR
+#define RESET_UNIT_SCRATCH_RAM_BASE_ADDR 0x80030400
+#endif
+
+#ifndef RESET_UNIT_SCRATCH_RAM_REG_ADDR
+#define RESET_UNIT_SCRATCH_RAM_REG_ADDR(n)                                                         \
+	(RESET_UNIT_SCRATCH_RAM_BASE_ADDR + sizeof(uint32_t) * (n))
+#endif
+
 /** @brief Magic identifier for Tenstorrent virtual UART (hex-speak for "TTSeRial") */
-#define UART_TT_VIRT_MAGIC      0x775e21a1
-#define UART_VIRT_DISCOVER_OFFS 0x000004A8
+#define UART_TT_VIRT_MAGIC 0x775e21a1
+
+#ifndef UART_TT_VIRT_DISCOVERY_ADDR
+#define UART_TT_VIRT_DISCOVERY_ADDR RESET_UNIT_SCRATCH_RAM_REG_ADDR(42)
+#endif
 
 /**
  * @brief In-memory ring buffer descriptor for Tenstorrent virtual UART.
@@ -37,33 +51,122 @@ extern "C" {
  *
  * TODO: add a nice ascii-art diagram.
  */
-struct uart_tt_virt_desc {
+struct tt_vuart {
 	uint32_t
 		magic; /**< Descriptor is initialized when `magic` equals @ref VIRTUAL_UART_MAGIC */
-	uint32_t tx_buf_capacity; /**< Transmit buffer capacity, in bytes */
-	uint32_t rx_buf_capacity; /**< Receive buffer capacity, in bytes */
-	uint32_t tx_head;         /** Transmit head counter */
-	uint32_t tx_tail;         /** Transmit tail counter */
-	uint32_t tx_oflow;        /** Number of transmit overflows (device to host) */
-	uint32_t rx_head;         /** Receive head counter */
-	uint32_t rx_tail;         /** Receive tail counter */
-	uint8_t buf[]; /** Buffer area of `tx_buf_size` bytes followed by `rx_buf_size` bytes */
+	uint32_t tx_cap;   /**< Transmit buffer capacity, in bytes */
+	uint32_t rx_cap;   /**< Receive buffer capacity, in bytes */
+	uint32_t tx_head;  /** Transmit head counter */
+	uint32_t tx_tail;  /** Transmit tail counter */
+	uint32_t tx_oflow; /** Number of transmit overflows (device to host) */
+	uint32_t rx_head;  /** Receive head counter */
+	uint32_t rx_tail;  /** Receive tail counter */
+	uint8_t buf[];     /** Buffer area of `tx_buf_size` bytes followed by `rx_buf_size` bytes */
 };
 
-static inline uint32_t uart_tt_virt_desc_buf_size(uint32_t head, uint32_t tail)
+enum tt_vuart_role {
+	TT_VUART_ROLE_DEVICE,
+	TT_VUART_ROLE_HOST,
+};
+
+static inline uint32_t tt_vuart_buf_size(uint32_t head, uint32_t tail)
 {
-	return (tail >= head) ? (tail - head) : (head - tail);
+	return tail - head;
 }
 
-static inline bool uart_tt_virt_desc_buf_empty(uint32_t head, uint32_t tail)
+static inline uint32_t tt_vuart_buf_space(uint32_t cap, uint32_t head, uint32_t tail)
 {
-	return uart_tt_virt_desc_buf_size(head, tail) == 0;
+	return cap - (tt_vuart_buf_size(head, tail));
 }
 
-static inline uint32_t uart_tt_virt_desc_buf_space(uint32_t capacity, uint32_t head, uint32_t tail)
+static inline bool tt_vuart_buf_empty(uint32_t head, uint32_t tail)
 {
-	return capacity - uart_tt_virt_desc_buf_size(head, tail);
+	return tt_vuart_buf_size(head, tail) == 0;
 }
+
+static inline int tt_vuart_poll_in(volatile struct tt_vuart *vuart, unsigned char *p_char,
+				   enum tt_vuart_role role)
+{
+	uint32_t cap;
+	uint32_t offs;
+	uint32_t tail;
+	uint32_t head;
+	volatile atomic_uint *headp;
+
+	do {
+		if (role == TT_VUART_ROLE_DEVICE) {
+			headp = (volatile atomic_uint *)&vuart->rx_head;
+			head = vuart->rx_head;
+			tail = vuart->rx_tail;
+			cap = vuart->rx_cap;
+			offs = vuart->tx_cap;
+		} else if (role == TT_VUART_ROLE_HOST) {
+			headp = (volatile atomic_uint *)&vuart->tx_head;
+			head = vuart->tx_head;
+			tail = vuart->tx_tail;
+			cap = vuart->tx_cap;
+			offs = 0;
+		} else {
+			/* assert? */
+		}
+
+		if (head == tail) {
+			/* if up-counters are equal, buffer is empty */
+			return -1;
+		}
+
+		if (atomic_compare_exchange_strong(headp, &head, head + 1)) {
+			*p_char = vuart->buf[offs + (head % cap)];
+		}
+	} while (true);
+}
+
+static inline void tt_vuart_poll_out(volatile struct tt_vuart *vuart, unsigned char out_char,
+				     enum tt_vuart_role role)
+{
+	uint32_t head;
+	uint32_t cap;
+	uint32_t offs;
+	uint32_t lim;
+	uint32_t tail;
+	volatile atomic_uint *tailp;
+
+	do {
+		if (role == TT_VUART_ROLE_DEVICE) {
+			tailp = (volatile atomic_uint *)&vuart->tx_tail;
+			tail = vuart->tx_tail;
+			head = vuart->tx_head;
+			cap = vuart->tx_cap;
+			offs = 0;
+		} else if (role == TT_VUART_ROLE_HOST) {
+			tailp = (volatile atomic_uint *)&vuart->rx_tail;
+			tail = vuart->rx_tail;
+			head = vuart->rx_head;
+			cap = vuart->rx_cap;
+			offs = vuart->tx_cap;
+		} else {
+			/* assert? */
+		}
+
+		lim = head + cap;
+
+		if ((role == TT_VUART_ROLE_DEVICE) && (tail == lim)) {
+			++vuart->tx_oflow;
+			break;
+		}
+
+		if (atomic_compare_exchange_strong(tailp, &tail, tail + 1)) {
+			vuart->buf[offs + tail % cap] = out_char;
+			break;
+		}
+	} while (true);
+}
+
+#ifdef __ZEPHYR__
+#include <zephyr/device.h>
+
+volatile struct tt_vuart *uart_tt_virt_get(const struct device *dev);
+#endif
 
 #ifdef __cplusplus
 }
