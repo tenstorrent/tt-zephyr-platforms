af08ac7416ad3589eabb74f1bc4054b9f8a04aad
Daniel DeGrasse <ddegrasse@tenstorrent.com>
2025-03-06 16:06:37 -0500

    lib: tenstorrent: bh_arc: remove spi controller code, use flash driver
    Remove the SPI eeprom driver code, and use the dw_flash driver. This
    simply removes some duplicate code, and relies directly on the dw flash
    driver for flash operations.
    
    Signed-off-by: Daniel DeGrasse <ddegrasse@tenstorrent.com>

--- lib/tenstorrent/bh_arc/spi_eeprom.h
+++ lib/tenstorrent/bh_arc/spi_eeprom.h
@@ -8,10 +8,7 @@
 
 #include <stdint.h>
 
-void SpiBufferSetup(void);
-void EepromSetup(void);
+void SpiEepromSetup(void);
 void ReinitSpiclk(void);
-void SpiBlockRead(uint32_t spi_address, uint32_t num_bytes, uint8_t *dest);
-void SpiSmartWrite(uint32_t address, const uint8_t *data, uint32_t num_bytes);
 
 #endif
