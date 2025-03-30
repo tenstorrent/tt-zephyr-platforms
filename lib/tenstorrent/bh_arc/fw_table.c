7aa1e1d3a896fca53e12855dc4325affe39cb271
Chris Friedt <cfriedt@tenstorrent.com>
2025-03-30 09:55:21 -0400

    bh_arc: fw_table: enable loading from spi with kconfig
    We do not flash fw tables during the normal CI process.
    
    This has the unfortunate consequence of introducing regressions
    if there is inadequate test coverage.
    
    If we use a Kconfig option to enable or disable loading the
    firmware table from SPI, then we can be sure there is no hidden
    state from one build to the next.
    
    This is a stop-gap solution until we have a complete
    unbrickable firmware update process, and a process by which
    we tightly couple the smc firmware image with firmware tables.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- lib/tenstorrent/bh_arc/fw_table.c
+++ lib/tenstorrent/bh_arc/fw_table.c
@@ -6,34 +6,144 @@
 
 #include "fw_table.h"
 
+#include <errno.h>
+#include <stdbool.h>
+#include <string.h>
+
 #include <pb_decode.h>
 #include <tenstorrent/tt_boot_fs.h>
+#include <zephyr/sys/util.h>
 
+#ifdef CONFIG_TT_FWTABLE_IN_SPI
 static FwTable fw_table;
+#else
+#if defined(BOARD_REVISION_P100)
+/*
+ * Manually extrapolated from
+ * boards/tenstorrent/tt_blackhole/spirom_data_tables/P100/fw_table.txt
+ */
+static FwTable fw_table = {
+	.fw_bundle_version = BUNDLE_VERSION_NUMBER,
+	.has_chip_limits = true,
+	.chip_limits = {
+		.asic_fmax = 1350,
+		.asic_fmin = 800,
+		.tdp_limit = 150,
+		.tdc_limit = 200,
+		.thm_limit = 90,
+		.tdc_fast_limit = 220,
+	},
+	.has_feature_enable = true,
+	.feature_enable = {
+		.cg_en = true,
+		/* FIXME: enabling the options below seems to break things */
+		.ddr_train_en = false,
+		.aiclk_ppm_en = false,
+	},
+	.has_pci1_property_table = true,
+	.pci1_property_table = {
+		.pcie_mode = FwTable_PciPropertyTable_PcieMode_EP,
+		.num_serdes = 2,
+	},
+	.has_eth_property_table = true,
+	.eth_property_table = {
+		.eth_disable_mask = 0x3fff,
+		.eth_disable_mask_en = true,
+	},
+};
+#elif defined(BOARD_REVISION_P100A)
+/*
+ * Manually extrapolated from
+ * boards/tenstorrent/tt_blackhole/spirom_data_tables/P100A/fw_table.txt
+ */
+static FwTable fw_table = {
+	.fw_bundle_version = BUNDLE_VERSION_NUMBER,
+	.has_chip_limits = true,
+	.chip_limits = {
+		.asic_fmax = 1350,
+		.asic_fmin = 800,
+		.tdp_limit = 150,
+		.tdc_limit = 200,
+		.thm_limit = 90,
+		.tdc_fast_limit = 220,
+	},
+	.has_feature_enable = true,
+	.feature_enable = {
+		.cg_en = true,
+		/* FIXME: enabling the options below seems to break things */
+		.ddr_train_en = false,
+		.aiclk_ppm_en = false,
+	},
+	.has_pci0_property_table = true,
+	.pci0_property_table = {
+		.pcie_mode = FwTable_PciPropertyTable_PcieMode_EP,
+		.num_serdes = 2,
+	},
+	.has_eth_property_table = true,
+	.eth_property_table = {
+		.eth_disable_mask = 0x3fff,
+		.eth_disable_mask_en = true,
+	},
+};
+#elif defined(BOARD_REVISION_P150A)
+/*
+ * Manually extrapolated from
+ * boards/tenstorrent/tt_blackhole/spirom_data_tables/P150A/fw_table.txt
+ */
+static FwTable fw_table = {
+	.fw_bundle_version = BUNDLE_VERSION_NUMBER,
+	.has_chip_limits = true,
+	.chip_limits = {
+		.asic_fmax = 1350,
+		.asic_fmin = 800,
+		.tdp_limit = 150,
+		.tdc_limit = 200,
+		.thm_limit = 90,
+		.tdc_fast_limit = 220,
+	},
+	.has_feature_enable = true,
+	.feature_enable = {
+		.cg_en = true,
+		/* FIXME: enabling the options below seems to break things */
+		.ddr_train_en = false,
+		.aiclk_ppm_en = false,
+	},
+	.has_pci0_property_table = true,
+	.pci0_property_table = {
+		.pcie_mode = FwTable_PciPropertyTable_PcieMode_EP,
+		.num_serdes = 2,
+	},
+};
+#else
+static FwTable fw_table = {
+	.fw_bundle_version = BUNDLE_VERSION_NUMBER,
+};
+#endif
+#endif
 
 /* Loader function that deserializes the fw table bin from the SPI filesystem */
 int load_fw_table(uint8_t *buffer_space, uint32_t buffer_size)
 {
-	static const char fwTableTag[TT_BOOT_FS_IMAGE_TAG_SIZE] = "cmfwcfg";
-	size_t bin_size = 0;
+	if (IS_ENABLED(CONFIG_TT_FWTABLE_IN_SPI)) {
+		static const char fwTableTag[TT_BOOT_FS_IMAGE_TAG_SIZE] = "cmfwcfg";
+		size_t bin_size = 0;
 
-	if (tt_boot_fs_get_file(&boot_fs_data, fwTableTag, buffer_space, buffer_size, &bin_size) !=
-	    TT_BOOT_FS_OK) {
-		/* Error */
-		/* TODO: Handle more gracefully */
-		return -1;
-	}
-	/* Convert the binary data to a pb_istream_t that is expected by decode */
-	pb_istream_t stream = pb_istream_from_buffer(buffer_space, bin_size);
-	/* PB_DECODE_NULLTERMINATED: Expect the message to be terminated with zero tag */
-	bool status = pb_decode_ex(&stream, &FwTable_msg, &fw_table, PB_DECODE_NULLTERMINATED);
+		if (tt_boot_fs_get_file(&boot_fs_data, fwTableTag, buffer_space, buffer_size,
+					&bin_size) != TT_BOOT_FS_OK) {
+			return -EIO;
+		}
+		/* Convert the binary data to a pb_istream_t that is expected by decode */
+		pb_istream_t stream = pb_istream_from_buffer(buffer_space, bin_size);
+		bool status =
+			pb_decode_ex(&stream, &FwTable_msg, &fw_table, PB_DECODE_NULLTERMINATED);
 
-	if (!status) {
-		/* Clear the table since a failed decode can leave it in an inconsistent state */
-		memset(&fw_table, 0, sizeof(fw_table));
-		/* Return -1 that should propagate up the stack */
-		return -1;
+		if (!status) {
+			/* Clear the table to avoid an inconsistent state */
+			memset(&fw_table, 0, sizeof(fw_table));
+			return -EIO;
+		}
 	}
+
 	return 0;
 }
 
