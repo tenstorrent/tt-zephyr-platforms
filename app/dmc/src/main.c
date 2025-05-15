33d2c66dc6d1bf7de1e6bd9a3e0635e5154cb006
Andrew Lewycky <alewycky@tenstorrent.com>
2025-05-15 17:58:03 -0400

    Switch to Zephyr's I2C driver

--- app/dmc/src/main.c
+++ app/dmc/src/main.c
@@ -238,6 +238,7 @@ int main(void)
 		}
 	}
 
+#if CONFIG_TT_SMBUS_DRIVER
 	ARRAY_FOR_EACH_PTR(BH_CHIPS, chip) {
 		if (chip->config.arc.smbus.bus == NULL) {
 			continue;
@@ -246,6 +247,7 @@ int main(void)
 		tt_smbus_stm32_set_abort_ptr(chip->config.arc.smbus.bus,
 					     &((&chip->data)->bus_cancel_flag));
 	}
+#endif
 
 	bist_rc = 0;
 	if (IS_ENABLED(CONFIG_TT_BIST)) {
