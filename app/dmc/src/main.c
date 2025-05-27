2de08810a5586b4d38f1b2dbf6b707f411155e70
Daniel Rosen <drosen@tenstorrent.com>
2025-05-27 11:37:04 -0400

    Ensure that the i2c driver is able to set the straps
    We need to use the arc i2c bus on the p300s to sets the straps
    which is how arc knows which ASIC is booting. Prior to this commit
    the bug clear flag was not being cleared which meant that the straps
    weren't being set.
    
    Signed-off-by: Daniel Rosen <drosen@tenstorrent.com>

--- app/dmc/src/main.c
+++ app/dmc/src/main.c
@@ -316,8 +316,8 @@ int main(void)
 				if (IS_ENABLED(CONFIG_TT_FAN_CTRL)) {
 					set_fan_speed(100);
 				}
-				bh_chip_reset_chip(chip, true);
 				bh_chip_cancel_bus_transfer_clear(chip);
+				bh_chip_reset_chip(chip, true);
 			}
 		}
 
@@ -325,6 +325,7 @@ int main(void)
 		ARRAY_FOR_EACH_PTR(BH_CHIPS, chip) {
 			if (chip->data.trigger_reset) {
 				chip->data.trigger_reset = false;
+				bh_chip_cancel_bus_transfer_clear(chip);
 				if (chip->data.workaround_applied) {
 					jtag_bootrom_reset_asic(chip);
 					jtag_bootrom_soft_reset_arc(chip);
@@ -333,7 +334,6 @@ int main(void)
 				} else {
 					chip->data.needs_reset = true;
 				}
-				bh_chip_cancel_bus_transfer_clear(chip);
 			}
 		}
 
