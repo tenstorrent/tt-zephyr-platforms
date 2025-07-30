41d3dab7721651614f7d69a72d84aec8012a9ce1
Daniel DeGrasse <ddegrasse@tenstorrent.com>
2025-07-30 11:00:18 -0400

    tests: drivers: flash_performance: make test use average times
    Take average of 10 iterations when programming or reading flash. This
    should improve CI stability, since one long programming duration won't
    cause test failure.
    
    Signed-off-by: Daniel DeGrasse <ddegrasse@tenstorrent.com>

--- tests/drivers/flash/performance/src/main.c
+++ tests/drivers/flash/performance/src/main.c
@@ -17,6 +17,7 @@
 #define TEST_AREA_SIZE		FIXED_PARTITION_SIZE(TEST_AREA)
 #define TEST_AREA_MAX		(TEST_AREA_OFFSET + TEST_AREA_SIZE)
 #define TEST_AREA_DEVICE	FIXED_PARTITION_DEVICE(TEST_AREA)
+#define TEST_ITERATIONS         10
 
 static const struct device *const flash_dev = TEST_AREA_DEVICE;
 static uint8_t buf[TEST_AREA_SIZE];
@@ -36,41 +37,49 @@ static int flash_program_wrap(const struct device *dev, off_t offset,
 ZTEST(flash_driver_perf, test_read_perf)
 {
 	int rc;
-	int64_t ts = k_uptime_get();
-	int64_t delta;
+	int64_t ts;
+	int64_t delta = 0;
 
-	rc = flash_read(flash_dev, TEST_AREA_OFFSET, buf, TEST_AREA_SIZE);
-	delta = k_uptime_delta(&ts);
-	zassert_equal(rc, 0, "Cannot read flash");
-	TC_PRINT("Read performance test ran in %lld ms\n", delta);
+	for (int i = 0; i < TEST_ITERATIONS; i++) {
+		ts = k_uptime_get();
+		rc = flash_read(flash_dev, TEST_AREA_OFFSET, buf, TEST_AREA_SIZE);
+		delta += k_uptime_delta(&ts);
+		zassert_equal(rc, 0, "Cannot read flash");
+	}
+	delta /= TEST_ITERATIONS;
+	TC_PRINT("Read performance test averaged %lld ms\n", delta);
 	zassert_true(delta < CONFIG_EXPECTED_READ_TIME, "Read performance test failed");
 }
 
 ZTEST(flash_driver_perf, test_program_perf)
 {
 	int rc;
-	int64_t ts = k_uptime_get();
-	int64_t delta;
+	int64_t ts;
+	int64_t delta = 0;
 
 	/* Create data to write to erased region */
 	for (int i = 0; i < TEST_AREA_SIZE; i++) {
 		buf[i] = (uint8_t)(i & 0xff);
 	}
 
-	/* Write buffer to flash */
-	ts = k_uptime_get();
-	rc = flash_program_wrap(flash_dev, TEST_AREA_OFFSET, buf, TEST_AREA_SIZE);
-	delta = k_uptime_delta(&ts);
-	zassert_equal(rc, 0, "Cannot program flash");
-	TC_PRINT("Program performance test ran in %lld ms\n", delta);
+	for (int i = 0; i < TEST_ITERATIONS; i++) {
+		/* Write buffer to flash */
+		ts = k_uptime_get();
+		rc = flash_program_wrap(flash_dev, TEST_AREA_OFFSET, buf, TEST_AREA_SIZE);
+		delta += k_uptime_delta(&ts);
+		zassert_equal(rc, 0, "Cannot program flash");
+		/* Read back the data */
+		rc = flash_read(flash_dev, TEST_AREA_OFFSET, check_buf, TEST_AREA_SIZE);
+		zassert_equal(rc, 0, "Cannot read flash");
+		/* Check that the data read back is the same as the data written */
+		zassert_mem_equal(buf, check_buf, TEST_AREA_SIZE,
+				  "Data read back from flash does not match data written");
+		TC_PRINT("Iteration %i: data read back from flash matches data written\n", i);
+	}
+	delta /= TEST_ITERATIONS;
+
+	TC_PRINT("Program performance test averaged %lld ms\n", delta);
 	zassert_true(delta < CONFIG_EXPECTED_PROGRAM_TIME, "Program performance test failed");
-	/* Read back the data */
-	rc = flash_read(flash_dev, TEST_AREA_OFFSET, check_buf, TEST_AREA_SIZE);
-	zassert_equal(rc, 0, "Cannot read flash");
-	/* Check that the data read back is the same as the data written */
-	zassert_mem_equal(buf, check_buf, TEST_AREA_SIZE,
-			  "Data read back from flash does not match data written");
-	TC_PRINT("Data read back from flash matches data written\n");
 }
 
 
