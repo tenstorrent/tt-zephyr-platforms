bae79b87962cbfe21b596b12b23205550ecb13bc
Aaron Fong <afong@tenstorrent.com>
2025-07-31 17:51:53 -0400

    bh_arc: regulator: add p300 and galaxy vcore and vcorem settings
    Signed-off-by: Aaron Fong <afong@tenstorrent.com>

--- lib/tenstorrent/bh_arc/regulator_config.c
+++ lib/tenstorrent/bh_arc/regulator_config.c
@@ -111,9 +111,185 @@ static const RegulatorData p1x0_vcorem_data[] = {
 
 /***** Start of p300 settings */
 
+/* VCORE */
+static const uint8_t p300_vcore_b0_data[] = {
+	0x00, 0x00, 0x3c, 0x00,
+	0x00, 0x03, 0x00, 0x00,
+	0x11, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00};
+static const uint8_t p300_vcore_b0_mask[] = {
+	0x00, 0x00, 0x3f, 0x00,
+	0x00, 0x1f, 0x00, 0x00,
+	0x1f, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00};
+
+BUILD_ASSERT(sizeof(p300_vcore_b0_data) == sizeof(p300_vcore_b0_mask));
+
+static const uint8_t p300_vcore_cb_data[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+static const uint8_t p300_vcore_cb_mask[] = {0x00, 0x07, 0x00, 0x00, 0x00, 0x00};
+
+BUILD_ASSERT(sizeof(p300_vcore_cb_data) == sizeof(p300_vcore_cb_mask));
+
+static const uint8_t p300_vcore_38_data[] = {0x02, 0x00};
+static const uint8_t p300_vcore_38_mask[] = {0xff, 0x07};
+
+BUILD_ASSERT(sizeof(p300_vcore_38_data) == sizeof(p300_vcore_38_mask));
+
+static const uint8_t p300_vcore_39_data[] = {0x02, 0x00};
+static const uint8_t p300_vcore_39_mask[] = {0xff, 0x07};
+
+BUILD_ASSERT(sizeof(p300_vcore_39_data) == sizeof(p300_vcore_39_mask));
+
+static const uint8_t p300_vcore_e7_data[] = {0x01};
+static const uint8_t p300_vcore_e7_mask[] = {0x07};
+
+BUILD_ASSERT(sizeof(p300_vcore_e7_data) == sizeof(p300_vcore_e7_mask));
+
+static const RegulatorData p300_vcore_data[] = {
+	REGULATOR_DATA(p300_vcore, b0),
+	REGULATOR_DATA(p300_vcore, cb),
+	REGULATOR_DATA(p300_vcore, 38),
+	REGULATOR_DATA(p300_vcore, 39),
+	REGULATOR_DATA(p300_vcore, e7),
+};
+
+/* VCOREM */
+static const uint8_t p300_vcorem_b0_data[] = {
+	0x00, 0x00, 0x2b, 0x00,
+	0x00, 0x07, 0x00, 0x00,
+	0x09, 0x00, 0x09, 0x00,
+	0x00, 0x00, 0x00, 0x00};
+static const uint8_t p300_vcorem_b0_mask[] = {
+	0x00, 0x00, 0x3f, 0x00,
+	0x00, 0x1f, 0x00, 0x00,
+	0x1f, 0x00, 0x0f, 0x00,
+	0x00, 0x00, 0x00, 0x00};
+
+BUILD_ASSERT(sizeof(p300_vcorem_b0_data) == sizeof(p300_vcorem_b0_mask));
+
+static const uint8_t p300_vcorem_38_data[] = {0x08, 0x00};
+static const uint8_t p300_vcorem_38_mask[] = {0xff, 0x07};
+
+BUILD_ASSERT(sizeof(p300_vcorem_38_data) == sizeof(p300_vcorem_38_mask));
+
+static const uint8_t p300_vcorem_39_data[] = {0x0c, 0x00};
+static const uint8_t p300_vcorem_39_mask[] = {0xff, 0x07};
+
+BUILD_ASSERT(sizeof(p300_vcorem_39_data) == sizeof(p300_vcorem_39_mask));
+
+static const uint8_t p300_vcorem_e7_data[] = {0x04};
+static const uint8_t p300_vcorem_e7_mask[] = {0x07};
+
+BUILD_ASSERT(sizeof(p300_vcorem_e7_data) == sizeof(p300_vcorem_e7_mask));
+
+static const RegulatorData p300_vcorem_data[] = {
+	REGULATOR_DATA(p300_vcorem, b0),
+	REGULATOR_DATA(p300_vcorem, 38),
+	REGULATOR_DATA(p300_vcorem, 39),
+	REGULATOR_DATA(p300_vcorem, e7),
+};
+
 /***** End of p300 settings */
 
 /***** Start of Galaxy/UBB settings */
+
+/* VCORE */
+static const uint8_t ubb_vcore_b0_data[] = {
+	0x00, 0x00, 0x00, 0x00,
+	0x00, 0x02, 0x06, 0x00,
+	0x11, 0x00, 0x00, 0x00,
+	0x00, 0x41, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00};
+static const uint8_t ubb_vcore_b0_mask[] = {
+	0x00, 0x00, 0x00, 0x00,
+	0x00, 0x1f, 0x0f, 0x00,
+	0x1f, 0x00, 0x00, 0x00,
+	0x00, 0x7f, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00};
+
+BUILD_ASSERT(sizeof(ubb_vcore_b0_data) == sizeof(ubb_vcore_b0_mask));
+
+static const uint8_t ubb_vcore_cb_data[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+static const uint8_t ubb_vcore_cb_mask[] = {0x00, 0x07, 0x00, 0x00, 0x00, 0x00};
+
+BUILD_ASSERT(sizeof(ubb_vcore_cb_data) == sizeof(ubb_vcore_cb_mask));
+
+static const uint8_t ubb_vcore_d3_data[] = {0x00};
+static const uint8_t ubb_vcore_d3_mask[] = {0x80};
+
+BUILD_ASSERT(sizeof(ubb_vcore_d3_data) == sizeof(ubb_vcore_d3_mask));
+
+static const uint8_t ubb_vcore_ca_data[] = {0x00, 0x78, 0x00, 0x00, 0x00};
+static const uint8_t ubb_vcore_ca_mask[] = {0x00, 0xff, 0x00, 0x00, 0x00};
+
+BUILD_ASSERT(sizeof(ubb_vcore_ca_data) == sizeof(ubb_vcore_ca_mask));
+
+static const uint8_t ubb_vcore_38_data[] = {0x02, 0x00};
+static const uint8_t ubb_vcore_38_mask[] = {0xff, 0x07};
+
+BUILD_ASSERT(sizeof(ubb_vcore_38_data) == sizeof(ubb_vcore_38_mask));
+
+static const uint8_t ubb_vcore_39_data[] = {0x02, 0x00};
+static const uint8_t ubb_vcore_39_mask[] = {0xff, 0x07};
+
+BUILD_ASSERT(sizeof(ubb_vcore_39_data) == sizeof(ubb_vcore_39_mask));
+
+static const uint8_t ubb_vcore_e7_data[] = {0x01};
+static const uint8_t ubb_vcore_e7_mask[] = {0x07};
+
+BUILD_ASSERT(sizeof(ubb_vcore_e7_data) == sizeof(ubb_vcore_e7_mask));
+
+static const RegulatorData ubb_vcore_data[] = {
+	REGULATOR_DATA(ubb_vcore, b0),
+	REGULATOR_DATA(ubb_vcore, cb),
+	REGULATOR_DATA(ubb_vcore, d3),
+	REGULATOR_DATA(ubb_vcore, ca),
+	REGULATOR_DATA(ubb_vcore, 38),
+	REGULATOR_DATA(ubb_vcore, 39),
+	REGULATOR_DATA(ubb_vcore, e7),
+};
+
+/* VCOREM */
+static const uint8_t ubb_vcorem_b0_data[] = {
+	0x00, 0x00, 0x2b, 0x00,
+	0x00, 0x07, 0x00, 0x00,
+	0x09, 0x00, 0x09, 0x00,
+	0x00, 0x00, 0x00, 0x00};
+static const uint8_t ubb_vcorem_b0_mask[] = {
+	0x00, 0x00, 0x3f, 0x00,
+	0x00, 0x1f, 0x00, 0x00,
+	0x1f, 0x00, 0x0f, 0x00,
+	0x00, 0x00, 0x00, 0x00};
+
+BUILD_ASSERT(sizeof(ubb_vcorem_b0_data) == sizeof(ubb_vcorem_b0_mask));
+
+static const uint8_t ubb_vcorem_38_data[] = {0x02, 0x00};
+static const uint8_t ubb_vcorem_38_mask[] = {0xff, 0x07};
+
+BUILD_ASSERT(sizeof(ubb_vcorem_38_data) == sizeof(ubb_vcorem_38_mask));
+
+static const uint8_t ubb_vcorem_39_data[] = {0x02, 0x00};
+static const uint8_t ubb_vcorem_39_mask[] = {0xff, 0x07};
+
+BUILD_ASSERT(sizeof(ubb_vcorem_39_data) == sizeof(ubb_vcorem_39_mask));
+
+static const uint8_t ubb_vcorem_e7_data[] = {0x04};
+static const uint8_t ubb_vcorem_e7_mask[] = {0x07};
+
+BUILD_ASSERT(sizeof(ubb_vcorem_e7_data) == sizeof(ubb_vcorem_e7_mask));
+
+static const RegulatorData ubb_vcorem_data[] = {
+	REGULATOR_DATA(ubb_vcorem, b0),
+	REGULATOR_DATA(ubb_vcorem, 38),
+	REGULATOR_DATA(ubb_vcorem, 39),
+	REGULATOR_DATA(ubb_vcorem, e7),
+};
 
 /* VOUT_SCALE_LOOP (0x29) = 444 (write word) */
 static const uint8_t ubb_gddrio_29_data[] = {0xbc, 0x01};
@@ -200,6 +376,16 @@ const BoardRegulatorsConfig p150_regulators_config = {
 
 static const RegulatorConfig p300_left_config[] = {
 	{
+		.address = P0V8_VCORE_ADDR,
+		.regulator_data = p300_vcore_data,
+		.count = ARRAY_SIZE(p300_vcore_data),
+	},
+	{
+		.address = P0V8_VCOREM_ADDR,
+		.regulator_data = p300_vcorem_data,
+		.count = ARRAY_SIZE(p300_vcorem_data),
+	},
+	{
 		.address = SERDES_VDDL_ADDR,
 		.regulator_data = serdes_vr_data,
 		.count = ARRAY_SIZE(serdes_vr_data),
@@ -218,6 +404,16 @@ const BoardRegulatorsConfig p300_left_regulators_config = {
 };
 
 static const RegulatorConfig p300_right_config[] = {
+	{
+		.address = P0V8_VCORE_ADDR,
+		.regulator_data = p300_vcore_data,
+		.count = ARRAY_SIZE(p300_vcore_data),
+	},
+	{
+		.address = P0V8_VCOREM_ADDR,
+		.regulator_data = p300_vcorem_data,
+		.count = ARRAY_SIZE(p300_vcorem_data),
+	},
 	{
 		.address = SERDES_VDDL_ADDR,
 		.regulator_data = serdes_vr_data,
@@ -241,6 +437,16 @@ const BoardRegulatorsConfig p300_right_regulators_config = {
 };
 
 static const RegulatorConfig ubb_config[] = {
+	{
+		.address = P0V8_VCORE_ADDR,
+		.regulator_data = ubb_vcore_data,
+		.count = ARRAY_SIZE(ubb_vcore_data),
+	},
+	{
+		.address = P0V8_VCOREM_ADDR,
+		.regulator_data = ubb_vcorem_data,
+		.count = ARRAY_SIZE(ubb_vcorem_data),
+	},
 	{
 		.address = GDDRIO_WEST_ADDR,
 		.regulator_data = ubb_gddrio_data,
