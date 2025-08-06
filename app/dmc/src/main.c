a59a2181f7ab9af58d3991a7e4c4852cd1b77929
Sherry Li <xiaoruli@tenstorrent.com>
2025-08-06 13:21:22 -0400

    fan_ctrl: enabling forced fan speed msg for both chips
    Signed-off-by: Sherry Li <xiaoruli@tenstorrent.com>

--- app/dmc/src/main.c
+++ app/dmc/src/main.c
@@ -30,7 +30,7 @@
 #include <tenstorrent/jtag_bootrom.h>
 #include <tenstorrent/tt_smbus_regs.h>
 
-#define RESET_UNIT_ARC_PC_CORE_0                0x80030C00
+#define RESET_UNIT_ARC_PC_CORE_0 0x80030C00
 
 LOG_MODULE_REGISTER(main, CONFIG_TT_APP_LOG_LEVEL);
 
@@ -47,6 +47,13 @@ static const struct device *const max6639_pwm_dev =
 	DEVICE_DT_GET_OR_NULL(DT_NODELABEL(max6639_pwm));
 static const struct device *const max6639_sensor_dev =
 	DEVICE_DT_GET_OR_NULL(DT_NODELABEL(max6639_sensor));
+
+#if BH_CHIP_COUNT == 2
+static uint8_t auto_fan_speed[BH_CHIP_COUNT] = {35, 35}; /* per–chip */
+#else
+static uint8_t auto_fan_speed[BH_CHIP_COUNT] = {35}; /* per–chip */
+#endif
+static uint8_t forced_fan_speed; /* 0-100 % */
 
 int update_fw(void)
 {
@@ -129,8 +136,25 @@ void process_cm2dm_message(struct bh_chip *chip)
 				uint8_t fan_speed_percentage = (uint8_t)message.data & 0xFF;
 				uint8_t fan_speed = (uint8_t)DIV_ROUND_UP(
 					fan_speed_percentage * UINT8_MAX, 100);
+				int chip_index = message.data >> 31;
 
 				pwm_set_cycles(max6639_pwm_dev, 0, UINT8_MAX, fan_speed, 0);
+				auto_fan_speed[chip_index] = fan_speed;
+			}
+			break;
+		case kCm2DmMsgIdForcedFanSpeedUpdate:
+			if (DT_NODE_HAS_STATUS(DT_ALIAS(fan0), okay)) {
+				uint8_t fan_speed_percentage = (uint8_t)message.data & 0xFF;
+				uint8_t fan_speed = (uint8_t)DIV_ROUND_UP(
+					fan_speed_percentage * UINT8_MAX, 100);
+				forced_fan_speed = fan_speed;
+
+				/* Broadcast forced speed to all CMFWs for telemetry */
+				for (int i = 0; i < BH_CHIP_COUNT; i++) {
+					bharc_smbus_word_data_write(&BH_CHIPS[i].config.arc,
+								    CMFW_SMBUS_FAN_SPEED,
+								    fan_speed_percentage);
+				}
 			}
 			break;
 		case kCm2DmMsgIdReady:
@@ -434,8 +458,7 @@ int main(void)
 				/* Read PC from ARC and record it */
 				jtag_setup(chip->config.jtag);
 				jtag_reset(chip->config.jtag);
-				jtag_axi_read32(chip->config.jtag,
-						RESET_UNIT_ARC_PC_CORE_0,
+				jtag_axi_read32(chip->config.jtag, RESET_UNIT_ARC_PC_CORE_0,
 						&chip->data.arc_hang_pc);
 				jtag_teardown(chip->config.jtag);
 				/* Clear watchdog state */
@@ -516,6 +539,17 @@ int main(void)
 
 			ARRAY_FOR_EACH_PTR(BH_CHIPS, chip) {
 				bh_chip_set_fan_rpm(chip, rpm);
+			}
+
+			if (forced_fan_speed != 0) {
+				pwm_set_cycles(max6639_pwm_dev, 0, UINT8_MAX, forced_fan_speed, 0);
+			} else {
+				uint8_t max_fan_speed = auto_fan_speed[0];
+
+				if (BH_CHIP_COUNT == 2) {
+					max_fan_speed = MAX(auto_fan_speed[0], auto_fan_speed[1]);
+				}
+				pwm_set_cycles(max6639_pwm_dev, 0, UINT8_MAX, max_fan_speed, 0);
 			}
 		}
 
