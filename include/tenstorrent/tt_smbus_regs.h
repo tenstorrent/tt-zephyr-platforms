8175890b14e38207ef002dae89392456c8a0e34c
James Growden <jgrowden@tenstorrent.com>
2025-08-06 13:25:58 -0400

    lib: tenstorrent: bh_arc: Add telem_read SMBUS message
    Add a SMBUS command to support reading telemetry data. This existed
    before, but not in the same format as on wormhole. This helps maintain
    compatability.
    
    To facilitate this, we need to support block-write-block-read SMBUS messages.
    
    Signed-off-by: James Growden <jgrowden@tenstorrent.com>

--- include/tenstorrent/tt_smbus_regs.h
+++ include/tenstorrent/tt_smbus_regs.h
@@ -14,6 +14,8 @@
  */
 
 enum CMFWSMBusReg {
+	/* RW, 8 bits in, 56 bits out. Get telemetry data */
+	CMFW_SMBUS_TELEMETRY_READ = 0x02,
 	/* W0, 24 bits. Update the Arc State */
 	CMFW_SMBUS_UPDATE_ARC_STATE = 0x04,
 	/* RO, 48 bits. Read cm2dmMessage struct describing request from CMFW */
