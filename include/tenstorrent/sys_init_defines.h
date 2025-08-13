ad31b38c4677619017dc395086b1343241becf8d
Nora Xu <nxu@tenstorrent.com>
2025-08-13 15:25:53 -0400

    sys_init: fix duplicate prios
    Change priorities of sys_init to avoid duplicates
    
    Signed-off-by: Nora Xu <nxu@tenstorrent.com>

--- include/tenstorrent/sys_init_defines.h
+++ include/tenstorrent/sys_init_defines.h
@@ -10,30 +10,30 @@
 #include <zephyr/init.h>
 
 /* SYS_INIT APPLICATION defines */
-#define register_interrupt_handlers_PRIO       0
-#define arc_dma_init_PRIO                      1
-#define InitSpiFS_PRIO                         2
-#define bh_arc_init_start_PRIO                 3
-#define CATEarlyInit_PRIO                      4
-#define CalculateHarvesting_PRIO               5
-#define DeassertTileResets_PRIO                6
-#define PLLInit_PRIO                           7
-#define PVTInit_PRIO                           8
-#define NocInit_PRIO                           9
-#define AssertSoftResets_PRIO                  10
-#define DeassertRiscvResets_PRIO               11
-#define InitAiclkPPM_PRIO                      12
-#define pcie_init_PRIO                         13
-#define InitMrisc_PRIO                         14
-#define eth_init_PRIO                          15
-#define InitSmbusTarget_PRIO                   16
-#define regulator_init_PRIO                    16
-#define avs_init_PRIO                          17
-#define tensix_cg_init_PRIO                    18
-#define InitNocTranslationFromHarvesting_PRIO  19
-#define gddr_training_PRIO                     20
-#define CATInit_PRIO                           20
-#define bh_arc_init_end_PRIO                   21
+#define register_interrupt_handlers_PRIO      0
+#define arc_dma_init_PRIO                     1
+#define InitSpiFS_PRIO                        2
+#define bh_arc_init_start_PRIO                3
+#define CATEarlyInit_PRIO                     4
+#define CalculateHarvesting_PRIO              5
+#define DeassertTileResets_PRIO               6
+#define PLLInit_PRIO                          7
+#define PVTInit_PRIO                          8
+#define NocInit_PRIO                          9
+#define AssertSoftResets_PRIO                 10
+#define DeassertRiscvResets_PRIO              11
+#define InitAiclkPPM_PRIO                     12
+#define pcie_init_PRIO                        13
+#define InitMrisc_PRIO                        14
+#define eth_init_PRIO                         15
+#define InitSmbusTarget_PRIO                  16
+#define regulator_init_PRIO                   17
+#define avs_init_PRIO                         18
+#define tensix_cg_init_PRIO                   19
+#define InitNocTranslationFromHarvesting_PRIO 20
+#define gddr_training_PRIO                    21
+#define CATInit_PRIO                          22
+#define bh_arc_init_end_PRIO                  23
 
 #define SYS_INIT_APP(func) SYS_INIT(func, APPLICATION, func##_PRIO)
 
