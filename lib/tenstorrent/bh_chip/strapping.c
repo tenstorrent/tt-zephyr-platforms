4e94090e7f666293cc86da27e52da2ce295f32cf
Sherry Li <xiaoruli@tenstorrent.com>
2025-08-19 12:24:05 -0400

    lib: bh_chip: add i2c recovery after failed strap configuration
    Signed-off-by: Sherry Li <xiaoruli@tenstorrent.com>

--- lib/tenstorrent/bh_chip/strapping.c
+++ lib/tenstorrent/bh_chip/strapping.c
@@ -9,9 +9,11 @@
 #include <tenstorrent/bh_chip.h>
 
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/i2c.h>
 
 void bh_chip_set_straps(struct bh_chip *chip)
 {
+	int ret;
 	bharc_enable_i2cbus(&chip->config.arc);
 	const struct gpio_dt_spec straps[] = {
 		chip->config.strapping.gpio6,
@@ -22,7 +24,22 @@ void bh_chip_set_straps(struct bh_chip *chip)
 
 	ARRAY_FOR_EACH_PTR(straps, strap_ptr) {
 		if (strap_ptr->port != NULL) {
-			gpio_pin_configure_dt(strap_ptr, GPIO_OUTPUT_ACTIVE);
+			ret = gpio_pin_configure_dt(strap_ptr, GPIO_OUTPUT_ACTIVE);
+			if (ret < 0) {
+				printk("Failed to configure strap %s: %d", strap_ptr->port->name,
+				       ret);
+				i2c_recover_bus(chip->config.arc.smbus.bus);
+				ret = gpio_pin_configure_dt(strap_ptr, GPIO_OUTPUT_ACTIVE);
+				if (ret < 0) {
+					printk("Failed to configure strap after i2c recover %s: "
+					       "%d\n",
+					       strap_ptr->port->name, ret);
+				} else {
+					printk("Strap %s successfully configured after i2c "
+					       "recover\n",
+					       strap_ptr->port->name);
+				}
+			}
 		}
 	}
 	bharc_disable_i2cbus(&chip->config.arc);
