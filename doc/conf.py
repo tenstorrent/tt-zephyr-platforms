7449237e2f7e82b6225a007d394520c966acd36b
Daniel DeGrasse <ddegrasse@tenstorrent.com>
2025-08-27 08:11:32 -0500

    ci: doc: deploy to github pages
    Deploy doc to github pages when pushed to main branch
    
    Signed-off-by: Daniel DeGrasse <ddegrasse@tenstorrent.com>
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- doc/conf.py
+++ doc/conf.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 TTZP = Path(__file__).parent.parent
 
-root_doc = "doc/index"
+root_doc = "index"
 project = "TT Zephyr Platforms"
 copyright = "2025, Tenstorrent AI ULC"
 author = "Tenstorrent AI ULC"
