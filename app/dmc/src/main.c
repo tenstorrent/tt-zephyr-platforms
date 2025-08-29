37ccb2af20afa06536c7dc97e4dab9d881e9105f
Andrew Lewycky <alewycky@tenstorrent.com>
2025-08-29 14:46:51 -0400

    Split process_cm2dm_message into per-message functions
    Message handling code has been growing as more messages have been
    added, and the next changes will complicate message management.
    
    Signed-off-by: Andrew Lewycky <alewycky@tenstorrent.com>

--- app/dmc/src/main.c
+++ app/dmc/src/main.c
@@ -108,86 +108,109 @@ int update_fw(void)
 	return ret;
 }
 
+static void process_reset_req(struct bh_chip *chip, uint8_t msg_id, uint32_t msg_data)
+{
+	switch (msg_data) {
+	case 0x0:
+		jtag_bootrom_reset_sequence(chip, true);
+		break;
+
+	case 0x3:
+		/* Trigger reboot; will reset asic and reload dmfw */
+		if (IS_ENABLED(CONFIG_REBOOT)) {
+			sys_reboot(SYS_REBOOT_COLD);
+		}
+		break;
+	}
+}
+
+static void process_ping(struct bh_chip *chip, uint8_t msg_id, uint32_t msg_data)
+{
+	/* Respond to ping request from CMFW */
+	bharc_smbus_word_data_write(&chip->config.arc, CMFW_SMBUS_PING, 0xA5A5);
+}
+
+static void process_fan_speed_update(struct bh_chip *chip, uint8_t msg_id, uint32_t msg_data)
+{
+	if (DT_NODE_HAS_STATUS(DT_ALIAS(fan0), okay)) {
+		uint8_t fan_speed_percentage = (uint8_t)msg_data & 0xFF;
+		uint8_t fan_speed = (uint8_t)DIV_ROUND_UP(fan_speed_percentage * UINT8_MAX, 100);
+		int chip_index = msg_data >> 31;
+
+		pwm_set_cycles(max6639_pwm_dev, 0, UINT8_MAX, fan_speed, 0);
+		auto_fan_speed[chip_index] = fan_speed;
+	}
+}
+
+static void process_forced_fan_speed_update(struct bh_chip *chip, uint8_t msg_id, uint32_t msg_data)
+{
+	if (DT_NODE_HAS_STATUS(DT_ALIAS(fan0), okay)) {
+		uint8_t fan_speed_percentage = (uint8_t)msg_data & 0xFF;
+		uint8_t fan_speed = (uint8_t)DIV_ROUND_UP(fan_speed_percentage * UINT8_MAX, 100);
+
+		forced_fan_speed = fan_speed;
+
+		/* Broadcast forced speed to all CMFWs for telemetry */
+		for (int i = 0; i < BH_CHIP_COUNT; i++) {
+			bharc_smbus_word_data_write(&BH_CHIPS[i].config.arc, CMFW_SMBUS_FAN_SPEED,
+						    fan_speed_percentage);
+		}
+	}
+}
+
+static void process_id_ready(struct bh_chip *chip, uint8_t msg_id, uint32_t msg_data)
+{
+	chip->data.arc_needs_init_msg = true;
+}
+
+static void process_auto_reset_timeout_update(struct bh_chip *chip, uint8_t msg_id
+					      uint32_t msg_data)
+{
+	/* Set auto reset timeout */
+	chip->data.auto_reset_timeout = msg_data;
+	if (chip->data.auto_reset_timeout != 0) {
+		/* Start auto-reset timer */
+		k_timer_start(&chip->auto_reset_timer, K_MSEC(chip->data.auto_reset_timeout),
+			      K_NO_WAIT);
+	} else {
+		/* Stop auto-reset timer */
+		k_timer_stop(&chip->auto_reset_timer);
+	}
+}
+
+static void process_heartbeat_update(struct bh_chip *chip, uint8_t msg_id, uint32_t msg_data)
+{
+	/* Update telemetry heartbeat */
+	if (chip->data.telemetry_heartbeat != msg_data) {
+		/* Telemetry heartbeat is moving */
+		chip->data.telemetry_heartbeat = msg_data;
+		if (chip->data.auto_reset_timeout != 0) {
+			/* Restart auto reset timer */
+			k_timer_start(&chip->auto_reset_timer,
+				      K_MSEC(chip->data.auto_reset_timeout), K_NO_WAIT);
+		}
+	}
+}
+
 void process_cm2dm_message(struct bh_chip *chip)
 {
+	typedef void (*msg_processor_t)(struct bh_chip *chip, uint8_t msg_id, uint32_t msg_data);
+
+	static const msg_processor_t msg_processors[] = {
+		[kCm2DmMsgIdResetReq] = process_reset_req,
+		[kCm2DmMsgIdPing] = process_ping,
+		[kCm2DmMsgIdFanSpeedUpdate] = process_fan_speed_update,
+		[kCm2DmMsgIdForcedFanSpeedUpdate] = process_forced_fan_speed_update,
+		[kCm2DmMsgIdReady] = process_id_ready,
+		[kCm2DmMsgIdAutoResetTimeoutUpdate] = process_auto_reset_timeout_update,
+		[kCm2DmMsgTelemHeartbeatUpdate] = process_heartbeat_update,
+	};
+
 	cm2dmMessageRet msg = bh_chip_get_cm2dm_message(chip);
 
 	if (msg.ret == 0) {
-		cm2dmMessage message = msg.msg;
-
-		switch (message.msg_id) {
-		case kCm2DmMsgIdResetReq:
-			switch (message.data) {
-			case 0x0:
-				jtag_bootrom_reset_sequence(chip, true);
-				break;
-			case 0x3:
-				/* Trigger reboot; will reset asic and reload dmfw
-				 */
-				if (IS_ENABLED(CONFIG_REBOOT)) {
-					sys_reboot(SYS_REBOOT_COLD);
-				}
-				break;
-			}
-			break;
-		case kCm2DmMsgIdPing:
-			/* Respond to ping request from CMFW */
-			bharc_smbus_word_data_write(&chip->config.arc, CMFW_SMBUS_PING, 0xA5A5);
-			break;
-		case kCm2DmMsgIdFanSpeedUpdate:
-			if (DT_NODE_HAS_STATUS(DT_ALIAS(fan0), okay)) {
-				uint8_t fan_speed_percentage = (uint8_t)message.data & 0xFF;
-				uint8_t fan_speed = (uint8_t)DIV_ROUND_UP(
-					fan_speed_percentage * UINT8_MAX, 100);
-				int chip_index = message.data >> 31;
-
-				pwm_set_cycles(max6639_pwm_dev, 0, UINT8_MAX, fan_speed, 0);
-				auto_fan_speed[chip_index] = fan_speed;
-			}
-			break;
-		case kCm2DmMsgIdForcedFanSpeedUpdate:
-			if (DT_NODE_HAS_STATUS(DT_ALIAS(fan0), okay)) {
-				uint8_t fan_speed_percentage = (uint8_t)message.data & 0xFF;
-				uint8_t fan_speed = (uint8_t)DIV_ROUND_UP(
-					fan_speed_percentage * UINT8_MAX, 100);
-				forced_fan_speed = fan_speed;
-
-				/* Broadcast forced speed to all CMFWs for telemetry */
-				for (int i = 0; i < BH_CHIP_COUNT; i++) {
-					bharc_smbus_word_data_write(&BH_CHIPS[i].config.arc,
-								    CMFW_SMBUS_FAN_SPEED,
-								    fan_speed_percentage);
-				}
-			}
-			break;
-		case kCm2DmMsgIdReady:
-			chip->data.arc_needs_init_msg = true;
-			break;
-		case kCm2DmMsgIdAutoResetTimeoutUpdate:
-			/* Set auto reset timeout */
-			chip->data.auto_reset_timeout = message.data;
-			if (chip->data.auto_reset_timeout != 0) {
-				/* Start auto-reset timer */
-				k_timer_start(&chip->auto_reset_timer,
-					      K_MSEC(chip->data.auto_reset_timeout), K_NO_WAIT);
-			} else {
-				/* Stop auto-reset timer */
-				k_timer_stop(&chip->auto_reset_timer);
-			}
-			break;
-		case kCm2DmMsgTelemHeartbeatUpdate:
-			/* Update telemetry heartbeat */
-			if (chip->data.telemetry_heartbeat != message.data) {
-				/* Telemetry heartbeat is moving */
-				chip->data.telemetry_heartbeat = message.data;
-				if (chip->data.auto_reset_timeout != 0) {
-					/* Restart auto reset timer */
-					k_timer_start(&chip->auto_reset_timer,
-						      K_MSEC(chip->data.auto_reset_timeout),
-						      K_NO_WAIT);
-				}
-			}
-			break;
+		if (msg.msg.msg_id < ARRAY_SIZE(msg_processors) && msg_processors[msg.msg.msg_id]) {
+			msg_processors[msg.msg.msg_id](chip, msg.msg.msg_id, msg.msg.data);
 		}
 	}
 }
