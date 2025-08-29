15b6e1a1c8a28b71288dc05f0089dd08ea269d2f
Andrew Lewycky <alewycky@tenstorrent.com>
2025-08-29 17:50:11 -0400

    Test that hard & soft float ABI are compatible for varargs functions
    Signed-off-by: Andrew Lewycky <alewycky@tenstorrent.com>

--- tests/lib/tenstorrent/bh_arc/src/fpu.c
+++ tests/lib/tenstorrent/bh_arc/src/fpu.c
@@ -8,7 +8,6 @@
 #include <zephyr/kernel.h>
 #include <math.h>
 
-
 /* picolib's floorf is compiled for soft float. This ensures that the ABI
  * is compatible with the hard float application.
  */
@@ -22,6 +21,18 @@ ZTEST(fpu, test_floorf)
 	k_float_enable(k_current_get(), 0);
 	zexpect_equal(floorf(1234.5f), 1234.0f);
 	zexpect_equal(p_floorf(2345.5f), 2345.0f);
+}
+
+/* Similarly, verify there's no problem with ABI compatibility for varargs functions. */
+ZTEST(fpu, test_float_snprintf)
+{
+	char buf[32];
+	float f = 1234.5f;
+	static const char expected[] = "1234.500";
+
+	k_float_enable(k_current_get(), 0);
+	zexpect_equal(snprintf(buf, sizeof(buf), "%.3f", (double)f), sizeof(expected) - 1);
+	zexpect_equal(strcmp(buf, expected), 0);
 }
 
 ZTEST_SUITE(fpu, NULL, NULL, NULL, NULL, NULL);
