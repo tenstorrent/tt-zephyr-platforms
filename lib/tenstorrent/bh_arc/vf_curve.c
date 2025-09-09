0735947c10bf9f6981734b8d5d13e1d55629bc4c
Alexander Lay <alexanderlay@tenstorrent.com>
2025-09-09 15:05:48 -0400

    lib: tenstorrent: bh_arc: add VF curve requests and message handlers
    Add request structs for VF curve messages and add to the tenstorrent
    host request union.
    
    Add message handlers for `MSG_TYPE_GET_VOLTAGE_CURVE_FROM_FREQ` and
    `MSG_TYPE_GET_FREQ_CURVE_FROM_VOLTAGE`.
    
    Signed-off-by: Alexander Lay <alexanderlay@tenstorrent.com>
    Signed-off-by: Sherry Li <xiaoruli@tenstorrent.com>

--- lib/tenstorrent/bh_arc/vf_curve.c
+++ lib/tenstorrent/bh_arc/vf_curve.c
@@ -5,8 +5,11 @@
  */
 
 #include <zephyr/sys/util.h>
+#include "aiclk_ppm.h"
 #include "vf_curve.h"
 #include <zephyr/drivers/misc/bh_fwtable.h>
+#include <tenstorrent/msgqueue.h>
+#include <tenstorrent/smc_msg.h>
 
 /* Bounds checks for frequency and voltage margin */
 #define FREQ_MARGIN_MAX    300.0F
@@ -18,8 +21,8 @@ static const float vf_quadratic_coeff = 0.00031395F;
 static const float vf_linear_coeff = -0.43953F;
 static const float vf_constant = 828.83F;
 
-static float freq_margin_mhz;
-static float voltage_margin_mv;
+static float freq_margin_mhz = FREQ_MARGIN_MAX;
+static float voltage_margin_mv = VOLTAGE_MARGIN_MAX;
 
 static const struct device *const fwtable_dev = DEVICE_DT_GET(DT_NODELABEL(fwtable));
 
@@ -47,3 +50,32 @@ float VFCurve(float freq_mhz)
 
 	return voltage_mv + voltage_margin_mv;
 }
+
+static uint8_t get_voltage_curve_from_freq_handler(const union request *request,
+						   struct response *response)
+{
+	float input_freq_mhz = (float)request->get_voltage_curve_from_freq.input_freq_mhz;
+	float voltage_mv = VFCurve(input_freq_mhz);
+
+	if (voltage_mv < 0.0F) {
+		response->data[1] = 0U;
+	} else {
+		response->data[1] = (uint32_t)(voltage_mv);
+	}
+
+	return 0;
+}
+
+static uint8_t get_freq_curve_from_voltage_handler(const union request *request,
+						   struct response *response)
+{
+	int input_voltage_mv = request->get_freq_curve_from_voltage.input_voltage_mv;
+	int freq_mhz = GetMaxAiclkForVoltage(input_voltage_mv);
+
+	response->data[1] = freq_mhz;
+
+	return 0;
+}
+
+REGISTER_MESSAGE(TT_SMC_MSG_GET_VOLTAGE_CURVE_FROM_FREQ, get_voltage_curve_from_freq_handler);
+REGISTER_MESSAGE(TT_SMC_MSG_GET_FREQ_CURVE_FROM_VOLTAGE, get_freq_curve_from_voltage_handler);
