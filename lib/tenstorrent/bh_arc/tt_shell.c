2537b04f310c96c440d9cc6bf44c2dbe0629ed22
James Growden <jgrowden@tenstorrent.com>
2025-09-15 11:29:38 -0400

    lib: bh_arc: Add ASIC state r/w to tt_shell
    Add the ability to get the current ASIC state via
    tt-shell. This was useful in debugging a Quanta issue.
    
    Signed-off-by: James Growden <jgrowden@tenstorrent.com>

--- lib/tenstorrent/bh_arc/tt_shell.c
+++ lib/tenstorrent/bh_arc/tt_shell.c
@@ -10,7 +10,27 @@
 
 #include "telemetry.h"
 #include "smbus_target.h"
+#include "asic_state.h"
 LOG_MODULE_REGISTER(tt_shell, CONFIG_LOG_DEFAULT_LEVEL);
+
+int asic_state_handler(const struct shell *sh, size_t argc, char **argv)
+{
+	if (argc == 2U) {
+		AsicState state = (AsicState)atoi(argv[1]);
+
+		if (state == A0State || state == A3State) {
+			set_asic_state(state);
+			shell_print(sh, "OK");
+		} else {
+			shell_error(sh, "Invalid ASIC State");
+			return -EINVAL;
+		}
+	} else {
+		shell_print(sh, "ASIC State: %u", get_asic_state());
+	}
+
+	return 0;
+}
 
 int telem_handler(const struct shell *sh, size_t argc, char **argv)
 {
@@ -50,6 +70,7 @@ int telem_handler(const struct shell *sh, size_t argc, char **argv)
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_tt_commands,
+			       SHELL_CMD_ARG(asic_state, NULL, "[|0|3]", asic_state_handler, 1, 1),
 			       SHELL_CMD_ARG(telem, NULL, "<Telemetry Index> [|x|f|d]",
 					     telem_handler, 2, 1),
 			       SHELL_SUBCMD_SET_END);
