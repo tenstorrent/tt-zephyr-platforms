9e4d4593a5f38e7903b09e8c403b226c7471d020
James Growden <jgrowden@tenstorrent.com>
2025-09-17 13:47:15 -0400

    lib: bh_arc: clang-format gddr.h
    Apply clang format to gddr.h
    
    Signed-off-by: James Growden <jgrowden@tenstorrent.com>

--- lib/tenstorrent/bh_arc/gddr.h
+++ lib/tenstorrent/bh_arc/gddr.h
@@ -27,19 +27,19 @@
 #define MRISC_POST_CODE                 RISC_CTRL_A_SCRATCH_1__REG_ADDR
 #define MRISC_MSG_REGISTER              RISC_CTRL_A_SCRATCH_2__REG_ADDR
 
-#define MRISC_INIT_FINISHED   0xdeadbeef
-#define MRISC_INIT_FAILED     0xfa11
-#define MRISC_INIT_BEFORE     0x11111111
-#define MRISC_INIT_STARTED    0x0
-#define MRISC_INIT_TIMEOUT    1000 /* In ms */
-#define MRISC_MEMTEST_TIMEOUT 1000 /* In ms */
+#define MRISC_INIT_FINISHED            0xdeadbeef
+#define MRISC_INIT_FAILED              0xfa11
+#define MRISC_INIT_BEFORE              0x11111111
+#define MRISC_INIT_STARTED             0x0
+#define MRISC_INIT_TIMEOUT             1000 /* In ms */
+#define MRISC_MEMTEST_TIMEOUT          1000 /* In ms */
 #define MRISC_POWER_SETTING_TIMEOUT_MS 1000
 
 /* Defined by MRISC FW */
-#define MRISC_MSG_TYPE_NONE        0
-#define MRISC_MSG_TYPE_PHY_POWERDOWN   1
-#define MRISC_MSG_TYPE_PHY_WAKEUP  2
-#define MRISC_MSG_TYPE_RUN_MEMTEST 8
+#define MRISC_MSG_TYPE_NONE          0
+#define MRISC_MSG_TYPE_PHY_POWERDOWN 1
+#define MRISC_MSG_TYPE_PHY_WAKEUP    2
+#define MRISC_MSG_TYPE_RUN_MEMTEST   8
 
 int read_gddr_telemetry_table(uint8_t gddr_inst, gddr_telemetry_table_t *gddr_telemetry);
 int32_t set_mrisc_power_setting(bool on);
