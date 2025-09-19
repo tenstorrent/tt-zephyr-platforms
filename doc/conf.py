2632d3620a7fb424513099b59b0ddaee4517d4a7
Chris Friedt <cfriedt@tenstorrent.com>
2025-09-19 10:02:02 -0400

    doc: derive documentation version from bundle / release version
    Derive the version displayed in generated documentation from the
    release / bundle VERSION file at the root of the repository.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2025 Tenstorrent AI ULC
 # SPDX-License-Identifier: Apache-2.0
 
+import sys
+
 from pathlib import Path
 import sys
 
@@ -15,10 +17,14 @@ sys.path.insert(0, str(ZEPHYR_BASE / "doc" / "_extensions"))
 # modules.
 sys.path.insert(0, str(ZEPHYR_BASE / "doc" / "_scripts"))
 
+sys.path.insert(0, str(TTZP / "scripts"))
+
+from get_ttzp_version import get_ttzp_version  # noqa: E402
+
 project = "TT Zephyr Platforms"
 copyright = "2025, Tenstorrent AI ULC"
 author = "Tenstorrent AI ULC"
-release = "1.0.0"
+release = get_ttzp_version()
 extensions = [
     "sphinx.ext.intersphinx",
     "sphinx_rtd_theme",
