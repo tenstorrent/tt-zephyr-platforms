c62199cce25bc0319368bbb1f9ba518bcd3370f5
Chris Friedt <cfriedt@tenstorrent.com>
2025-09-25 07:34:26 -0400

    scripts: tooling: logging: rework macros and add rate-limited variants
    Rework the logging macros for better code reuse, add a file designator,
    and write to stderr as appropriate.
    
    Add rate-limited versions of macros to avoid spamming logs.
    
    Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

--- scripts/tooling/logging.h
+++ scripts/tooling/logging.h
@@ -6,19 +6,48 @@
 #ifndef SCRIPTS_TOOLING_LOGGING_H_
 #define SCRIPTS_TOOLING_LOGGING_H_
 
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+
 /* Should be defined and set by main program */
 extern int verbose;
 
-#define D(level, fmt, ...)                                                                         \
-	if (verbose >= level) {                                                                    \
-		printf("D: %s(): " fmt "\n", __func__, ##__VA_ARGS__);                             \
+#define _log(file, fmt, ...) fprintf(file, fmt "\n", ##__VA_ARGS__)
+
+#define _log_rl(msec, file, fmt, ...)                                                              \
+	{                                                                                          \
+		static uint64_t last_log_ms;                                                       \
+		struct timespec ts;                                                                \
+		uint64_t now;                                                                      \
+                                                                                                   \
+		clock_gettime(CLOCK_REALTIME, &ts);                                                \
+		now = ts.tv_sec * 1000 + ts.tv_nsec / 1000000;                                     \
+		if ((now - last_log_ms) >= (uint64_t)(msec)) {                                     \
+			_log(file, fmt "\n", ##__VA_ARGS__);                                       \
+			last_log_ms = now;                                                         \
+		}                                                                                  \
 	}
 
-#define E(fmt, ...) fprintf(stderr, "E: %s(): " fmt "\n", __func__, ##__VA_ARGS__)
+#define D(level, fmt, ...)                                                                         \
+	if (verbose >= (level)) {                                                                  \
+		_log(stdout, "D: %s(): " fmt, __func__, ##__VA_ARGS__);                            \
+	}
+
+#define E(fmt, ...) _log(stderr, "E: %s(): " fmt, __func__, ##__VA_ARGS__)
 
 #define I(fmt, ...)                                                                                \
 	if (verbose >= 0) {                                                                        \
-		printf(fmt "\n", ##__VA_ARGS__);                                                   \
+		_log(stdout, fmt, ##__VA_ARGS__);                                                  \
 	}
+
+#define D_RL(level, msec, fmt, ...)                                                                \
+	if (verbose >= (level)) {                                                                  \
+		_log_rl((msec), stdout, "D: %s(): " fmt, __func__, ##__VA_ARGS__);                 \
+	}
+
+#define E_RL(msec, fmt, ...) _log_rl((msec), stderr, "E: %s(): " fmt, __func__, ##__VA_ARGS__);
+
+#define I_RL(msec, fmt, ...) _log_rl((msec), stdout, fmt, __func__, ##__VA_ARGS__);
 
 #endif /* SCRIPTS_TOOLING_LOGGING_H_ */
