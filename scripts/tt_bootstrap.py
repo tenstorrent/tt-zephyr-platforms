db4cdaaa38128b77e532c1766f5f5fe9553295d5
Daniel DeGrasse <ddegrasse@tenstorrent.com>
2025-10-02 23:58:15 -0500

    scripts: tt_bootstrap: place headers on binary file to flash it directly
    Place headers on a binary file to flash it directly to the SPI flash.
    This can be useful to load firmware that does not build a full firmware
    bundle into SPI flash.
    
    Signed-off-by: Daniel DeGrasse <ddegrasse@tenstorrent.com>

--- scripts/tt_bootstrap.py
+++ scripts/tt_bootstrap.py
@@ -39,6 +39,8 @@ if os.environ.get("PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION") != "python":
     os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 from encode_spirom_bins import convert_proto_txt_to_bin_file
 
+TT_Z_P_ROOT = Path(__file__).parents[1]
+
 try:
     from pyocd.core.helpers import ConnectHelper
     from pyocd.flash.file_programmer import FileProgrammer
@@ -75,6 +77,7 @@ class TTBootStrapRunner(ZephyrBinaryRunner):
         self.pyocd_path = (
             Path(__file__).parent / "tooling/blackhole_recovery/data/bh_flm"
         )
+        self.should_rescan = False
         if self.board_name not in BOARD_ID_MAP:
             raise ValueError(
                 f"Unknown board name: {self.board_name}. Supported names: "
@@ -90,7 +93,7 @@ class TTBootStrapRunner(ZephyrBinaryRunner):
         # We support flashing hex or binary files directly. Otherwise, we will
         # extract the firmware from fwbundle
         if bootfs_hex:
-            logger.info(
+            self.logger.info(
                 "Hex file provided, using this directly (board ID argument ignored)"
             )
             # Load the hex file as data to write
@@ -100,13 +103,20 @@ class TTBootStrapRunner(ZephyrBinaryRunner):
                 ]
             except FileNotFoundError as e:
                 raise RuntimeError(f"Hex file {bootfs_hex} does not exist") from e
+        elif fw_bundle:
+            self.flash_data = self.parse_fwbundle(fw_bundle)
+            self.should_rescan = True
+        elif (Path(cfg.build_dir).parent / "update.fwbundle").exists():
+            self.flash_data = self.parse_fwbundle(
+                str(Path(cfg.build_dir).parent / "update.fwbundle")
+            )
         else:
-            if fw_bundle:
-                self.flash_data = self.parse_fwbundle(fw_bundle)
-            else:
-                self.flash_data = self.parse_fwbundle(
-                    str(Path(cfg.build_dir).parent / "update.fwbundle")
-                )
+            # Use the binary file, and wrap it in a tt-boot-fs structure
+            # so the bootrom will execute it correctly
+            self.logger.warning(
+                "No firmware bundle provided, looking for binary file in build directory"
+            )
+            self.flash_data = self.parse_bin(cfg.bin_file, asic_id)
 
     @classmethod
     def name(cls):
@@ -151,6 +161,87 @@ class TTBootStrapRunner(ZephyrBinaryRunner):
             args.asic_id,
             args.adapter_id,
         )
+
+    def parse_bin(self, bin_file, asic_id):
+        """
+        Parses a binary file and wraps it in a bootfs structure for flashing.
+        """
+        # First address for data
+        offset = 0x14000  # After SPI training word
+        cfg = BOARD_ID_MAP[self.board_name][asic_id]
+        # We *really* should not need all these files to be present to run
+        # applications, but the BH_ARC library has a broad set of dependencies
+        file_paths = {
+            "boardcfg": self.build_dir
+            / "generated_board_cfg"
+            / cfg["protobuf-name"]
+            / "read_only.bin",
+            "flshinfo": self.build_dir
+            / "generated_board_cfg"
+            / cfg["protobuf-name"]
+            / "flash_info.bin",
+            "cmfwcfg": self.build_dir
+            / "generated_board_cfg"
+            / cfg["protobuf-name"]
+            / "fw_table.bin",
+            "ethfw": TT_Z_P_ROOT / "zephyr" / "blobs" / "tt_blackhole_erisc.bin",
+            "ethfwcfg": TT_Z_P_ROOT
+            / "zephyr"
+            / "blobs"
+            / "tt_blackhole_erisc_params.bin",
+            "memfw": TT_Z_P_ROOT / "zephyr" / "blobs" / "tt_blackhole_gddr_init.bin",
+            "memfwcfg": TT_Z_P_ROOT
+            / "zephyr"
+            / "blobs"
+            / f"tt_blackhole_gddr_params_{self.board_name.upper()}.bin",
+            "ethsdreg": TT_Z_P_ROOT
+            / "zephyr"
+            / "blobs"
+            / "tt_blackhole_serdes_eth_fwreg.bin",
+            "ethsdfw": TT_Z_P_ROOT
+            / "zephyr"
+            / "blobs"
+            / "tt_blackhole_serdes_eth_fw.bin",
+        }
+        # CMFW should be first entry, so the bootrom finds it and jumps to
+        # it before copying other tags to invalid ICCM addresses.
+        order = ["cmfw"] + list(file_paths.keys())
+        try:
+            with open(bin_file, "rb") as f:
+                data = f.read()
+        except FileNotFoundError as e:
+            raise RuntimeError(f"Binary file {bin_file} does not exist") from e
+
+        # Create cmfw entry
+        bootfs_entries = {
+            "cmfw": tt_boot_fs.FsEntry(False, "cmfw", data, offset, 0x10000000, True)
+        }
+        # Update offset, round up to 0x1000 boundary
+        offset += (len(data) + 0xFFF) & ~0xFFF
+        # Create entries for the required config files
+        for path, name in zip(file_paths.values(), file_paths.keys()):
+            if not Path(path).exists():
+                raise RuntimeError(f"Required file {name} not found at {path}")
+            with open(path, "rb") as f:
+                file_data = f.read()
+                bootfs_entries[name] = tt_boot_fs.FsEntry(
+                    False, name, file_data, offset, 0x0, False
+                )
+                offset += (len(file_data) + 0xFFF) & ~0xFFF
+        # Pull the board configuration files into the tt-boot-fs image
+        # Create empty failover entry at 0x5000 (in recovery image region)
+        failover = tt_boot_fs.FsEntry(False, "failover", b"", 0x5000, 0x10000000, True)
+        bootfs = tt_boot_fs.BootFs(order, bootfs_entries, failover)
+        # Write out the bootfs binary for debugging
+        with open(self.build_dir / "bootfs.bin", "wb") as f:
+            self.logger.debug("Writing bootfs binary to build directory")
+            f.write(bootfs.to_binary())
+        operations = [
+            FlashOperation(
+                bootfs.to_intel_hex(), self.pyocd_path / Path(cfg["pyocd-config"])
+            )
+        ]
+        return operations
 
     def parse_fwbundle(self, fw_bundle):
         """
@@ -270,20 +361,21 @@ class TTBootStrapRunner(ZephyrBinaryRunner):
             target.reset_and_halt()
             target.resume()
             session.close()
+        # If flashing a firmware bundle, we may need to wait for DMC update.
+        if self.should_rescan:
             # Rescan PCIe bus
             self.logger.info(
                 "Waiting up to 60 seconds for the DMC to complete the update"
             )
             timeout = 60  # Timeout in seconds
             start_time = time.time()
-            while True:
+            while time.time() - start_time > timeout:
                 pcie_utils.rescan_pcie()
                 if len(pcie_utils.find_tt_devs()) == len(self.flash_data):
                     # All devices found, exit loop and flash next ASIC
                     self.logger.info("Device reappeared after rescan")
                     break
-                if time.time() - start_time > timeout:
-                    self.logger.error("Timeout expired while waiting for PCIe rescan.")
-                    break
                 # Throttle rescan attempts
                 time.sleep(1)
+            if time.time() - start_time > timeout:
+                raise RuntimeError("Timeout waiting for device to reappear after flash")
