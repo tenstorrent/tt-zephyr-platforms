5872531997510a2c8ffec6980498a53127cdf7b3
James Growden <jgrowden@tenstorrent.com>
2025-10-15 12:59:52 -0400

    app: smc: pytest: Add FW upgrade test
    Add a stress test that upgrades the FW from 18.x to mainline firmware.
    
    Signed-off-by: James Growden <jgrowden@tenstorrent.com>

--- scripts/ci/run-smoke.sh
+++ scripts/ci/run-smoke.sh
@@ -49,9 +49,10 @@ fi
 
 CONSOLE_DEV="/dev/tenstorrent/$ASIC_ID"
 
-# Export ASIC_ID and console dev as environment variables for use by scripts
+# Export ASIC_ID, BOARD, and console dev as environment variables for use by scripts
 export ASIC_ID
 export CONSOLE_DEV
+export BOARD
 
 if [ -z "$TEST_SET" ]; then
     TEST_SET=":dmc:smc"
