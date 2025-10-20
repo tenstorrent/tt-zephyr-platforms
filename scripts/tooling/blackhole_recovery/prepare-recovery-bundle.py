987a76d397919f1f6077a9a4e0afccf5d4e3196d
Daniel DeGrasse <ddegrasse@tenstorrent.com>
2025-10-20 17:29:08 -0500

    scripts: tooling: blackhole_recovery: support building per board
    Support building blackhole recovery tooling for singular boards, so
    builds can run in parallel and be combined later.
    
    Signed-off-by: Daniel DeGrasse <ddegrasse@tenstorrent.com>

--- scripts/tooling/blackhole_recovery/prepare-recovery-bundle.py
+++ scripts/tooling/blackhole_recovery/prepare-recovery-bundle.py
@@ -53,6 +53,9 @@ def parse_args():
         "bundle_out",
         help="Output filename for recovery bundle",
     )
+    parser.add_argument(
+        "--board", help="Board to build for (default: all boards)", default="all"
+    )
     return parser.parse_args()
 
 
@@ -132,8 +135,18 @@ def generate_recovery_assets(boardname, board_data, outdir):
 def main():
     args = parse_args()
     with tempfile.TemporaryDirectory() as temp_dir:
-        for board in BOARD_ID_MAP:
-            generate_recovery_assets(board, BOARD_ID_MAP[board], Path(temp_dir) / board)
+        if args.board != "all":
+            if args.board not in BOARD_ID_MAP:
+                print(f"Error: board '{args.board}' is not recognized")
+                sys.exit(1)
+            generate_recovery_assets(
+                args.board, BOARD_ID_MAP[args.board], Path(temp_dir) / args.board
+            )
+        else:
+            for board in BOARD_ID_MAP:
+                generate_recovery_assets(
+                    board, BOARD_ID_MAP[board], Path(temp_dir) / board
+                )
         # Build the pyocd flash algorithms, so we can include them in the bundle
         cmd = [
             "cmake",
