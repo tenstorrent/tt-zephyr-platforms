944910e00c2d0bd455c65afc1a9e5ad3169d1ab0
Alex Apostolu <aapostolu@tenstorrent.com>
2025-10-30 12:44:18 -0400

    lib: tenstorrent: bh_arc: replace old dma library with zephyr dma
    Replaced the old DMA library with the new Zephyr DMA driver.
    
    Signed-off-by: Alex Apostolu <aapostolu@tenstorrent.com>

--- lib/tenstorrent/bh_arc/tensix_init.c
+++ lib/tenstorrent/bh_arc/tensix_init.c
@@ -13,6 +13,8 @@
 #include <tenstorrent/post_code.h>
 #include <tenstorrent/sys_init_defines.h>
 #include <zephyr/drivers/misc/bh_fwtable.h>
+#include <zephyr/drivers/dma.h>
+#include <zephyr/drivers/dma/dma_tt_bh_noc.h>
 #include <zephyr/init.h>
 
 #define ARC_NOC0_X 8
@@ -21,6 +23,7 @@
 #define TENSIX_L1_SIZE (1536 * 1024)
 
 static const struct device *const fwtable_dev = DEVICE_DT_GET(DT_NODELABEL(fwtable));
+static const struct device *const dma_noc = DEVICE_DT_GET_OR_NULL(DT_NODELABEL(dma1));
 
 /* Enable CG_CTRL_EN in each non-harvested Tensix node and set CG hystersis to 2. */
 /* This requires NOC init so that broadcast is set up properly. */
@@ -83,8 +86,29 @@ static void wipe_l1(void)
 
 	/* wipe SCRATCHPAD_SIZE of the chosen tensix */
 	memset(sram_buffer, 0, sizeof(sram_buffer));
-	noc_dma_read(tensix_x, tensix_y, addr, ARC_NOC0_X, ARC_NOC0_Y, (uintptr_t)sram_buffer,
-		     sizeof(sram_buffer), true);
+
+	struct tt_bh_dma_noc_coords coords =
+		tt_bh_dma_noc_coords_init(tensix_x, tensix_y, ARC_NOC0_X, ARC_NOC0_Y);
+
+	struct dma_block_config block = {
+		.source_address = addr,
+		.dest_address = (uintptr_t)sram_buffer,
+		.block_size = sizeof(sram_buffer),
+	};
+
+	struct dma_config config = {
+		.channel_direction = MEMORY_TO_PERIPHERAL,
+		.source_data_size = 1,
+		.dest_data_size = 1,
+		.source_burst_length = 1,
+		.dest_burst_length = 1,
+		.block_count = 1,
+		.head_block = &block,
+		.user_data = &coords,
+	};
+
+	dma_config(dma_noc, 1, &config);
+	dma_start(dma_noc, 1);
 
 	/* wipe entire L1 of the chosen tensix */
 	uint32_t offset = sizeof(sram_buffer);
@@ -92,12 +116,26 @@ static void wipe_l1(void)
 	while (offset < TENSIX_L1_SIZE) {
 		uint32_t size = MIN(offset, TENSIX_L1_SIZE - offset);
 
-		noc_dma_write(tensix_x, tensix_y, addr, tensix_x, tensix_y, offset, size, true);
+		config.channel_direction = PERIPHERAL_TO_MEMORY;
+		coords.dest_x = tensix_x;
+		coords.dest_y = tensix_y;
+		block.dest_address = offset;
+		block.block_size = size;
+
+		dma_config(dma_noc, 1, &config);
+		dma_start(dma_noc, 1);
+
 		offset += offset;
 	}
 
 	/* clear all remaining tensix L1 using the already-cleared L1 as a source */
-	noc_dma_broadcast(tensix_x, tensix_y, addr, TENSIX_L1_SIZE);
+	config.channel_direction = TT_BH_DMA_NOC_CHANNEL_DIRECTION_BROADCAST;
+	block.source_address = addr;
+	block.dest_address = addr;
+	block.block_size = TENSIX_L1_SIZE;
+
+	dma_config(dma_noc, 1, &config);
+	dma_start(dma_noc, 1);
 }
 
 void TensixInit(void)
