c839ddd482224a35d2fd4bbcbdfb670b0829fc98
Daniel DeGrasse <ddegrasse@tenstorrent.com>
2026-01-15 16:34:18 -0600

    soc: tenstorrent: tt_grendel: add i3c init
    Add i3c init code to take I3C peripheral out of reset.
    
    Signed-off-by: Daniel DeGrasse <ddegrasse@tenstorrent.com>

--- soc/tenstorrent/tt_grendel/tt_grendel_smc/soc.c
+++ soc/tenstorrent/tt_grendel/tt_grendel_smc/soc.c
@@ -12,11 +12,18 @@
 #define UART_INIT_IF_OKAY(n)                                                                       \
 	do {                                                                                       \
 		if (DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(uart##n))) {                              \
-			uart_init(UART_WRAP##n##_UART_CTRL_REG_ADDR);                              \
+			uart_x_init(UART_WRAP##n##_UART_CTRL_REG_ADDR);                            \
 		}                                                                                  \
 	} while (false)
 
-static void uart_init(uint32_t base_addr)
+#define I3C_INIT_IF_OKAY(n)                                                                        \
+	do {                                                                                       \
+		if (DT_NODE_HAS_STATUS_OKAY(DT_NODELABEL(i3c_##n))) {                              \
+			i3c_x_init(I3C_WRAP_##n##_I3C_CTRL_REG_MAP_BASE_ADDR);                     \
+		}                                                                                  \
+	} while (false)
+
+static void uart_x_init(uint32_t base_addr)
 {
 	UART_CTRL_reg_u uart_ctrl;
 
@@ -24,6 +31,46 @@ static void uart_init(uint32_t base_addr)
 	uart_ctrl.f.uart_en = 1;
 	uart_ctrl.f.uart_reset_n_n0_scan = 1;
 	sys_write32(uart_ctrl.val, base_addr);
+}
+
+static void i3c_x_init(uint32_t i3c_base_addr)
+{
+	I3C_CTRL_I3C_RESET_CTRL_STATUS_reg_u i3c_ctrl;
+	I3C_CTRL_PINSTRAPS_GROUP_1A_reg_u i3c_pinstraps;
+
+	/* Configure I3C role as controller */
+	i3c_pinstraps.val =
+		sys_read32(i3c_base_addr + I3C_WRAP_0_I3C_CTRL_PINSTRAPS_GROUP_1A_REG_OFFSET);
+	i3c_pinstraps.f.device_role = 0x0; /* Primary Controller */
+	sys_write32(i3c_pinstraps.val,
+		    i3c_base_addr + I3C_WRAP_0_I3C_CTRL_PINSTRAPS_GROUP_1A_REG_OFFSET);
+
+	/* Enable I3Cx */
+	i3c_ctrl.val =
+		sys_read32(i3c_base_addr + I3C_WRAP_0_I3C_CTRL_I3C_RESET_CTRL_STATUS_REG_OFFSET);
+	i3c_ctrl.f.i3c_reset_n_n0_scan = 1;
+	i3c_ctrl.f.reg_reset_n_n0_scan = 1;
+	i3c_ctrl.f.i3c_enable_n0_scan = 1;
+	sys_write32(i3c_ctrl.val,
+		    i3c_base_addr + I3C_WRAP_0_I3C_CTRL_I3C_RESET_CTRL_STATUS_REG_OFFSET);
+}
+
+void uart_init(void)
+{
+	UART_INIT_IF_OKAY(0);
+	UART_INIT_IF_OKAY(1);
+	UART_INIT_IF_OKAY(2);
+	UART_INIT_IF_OKAY(3);
+}
+
+void i3c_init(void)
+{
+	I3C_INIT_IF_OKAY(0);
+	I3C_INIT_IF_OKAY(1);
+	I3C_INIT_IF_OKAY(2);
+	I3C_INIT_IF_OKAY(3);
+	I3C_INIT_IF_OKAY(4);
+	I3C_INIT_IF_OKAY(5);
 }
 
 /**
@@ -37,11 +84,10 @@ void soc_early_init_hook(void)
 
 	/* Take peripherals out of reset */
 	reset_reg.f.uart_reset_n_n0_scan = DT_HAS_COMPAT_STATUS_OKAY(ns16550);
+	reset_reg.f.i3c_reset_n_n0_scan = DT_HAS_COMPAT_STATUS_OKAY(cdns_i3c);
 
 	sys_write32(reset_reg.val, RESET_UNIT_PERIPHERAL_RESETS_REG_ADDR);
 
-	UART_INIT_IF_OKAY(0);
-	UART_INIT_IF_OKAY(1);
-	UART_INIT_IF_OKAY(2);
-	UART_INIT_IF_OKAY(3);
+	uart_init();
+	i3c_init();
 }
