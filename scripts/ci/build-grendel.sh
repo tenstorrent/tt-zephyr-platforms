f666da5b2d32b0ad55c5e4592773e37321017202
Daniel DeGrasse <ddegrasse@tenstorrent.com>
2026-02-04 09:08:34 -0600

    wip: attempting to get build artifacts into the right directory...
    Signed-off-by: Daniel DeGrasse <ddegrasse@tenstorrent.com>

--- scripts/ci/build-grendel.sh
+++ scripts/ci/build-grendel.sh
@@ -6,5 +6,7 @@
 # This script builds grendel platforms within the tt-zephyr-platforms CI
 # image environment.
 
+OUTDIR=${OUTDIR:-twister-out}
+
 # Build all grendel platforms
-./zephyr/scripts/twister -p tt_grendel_smc -T ./zephyr/samples/hello_world
+./zephyr/scripts/twister -p tt_grendel_smc -T ./zephyr/samples/hello_world --outdir $OUTDIR
