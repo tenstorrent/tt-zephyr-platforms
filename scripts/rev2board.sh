227d4af600cd949383458b87600c07ff3d679f80
Alexander Lay <alexanderlay@tenstorrent.com>
2026-03-05 12:57:41 -0500

    ci: add orion_slt board type
    Add support for recognizing Orion SLT board to trigger build firmware job.
    
    Signed-off-by: Alexander Lay <alexanderlay@tenstorrent.com>

--- scripts/rev2board.sh
+++ scripts/rev2board.sh
@@ -18,15 +18,8 @@ if [ $# -ge 2 ]; then
 fi
 
 case "$1" in
-  p100a) BOARD="tt_blackhole@p100a/tt_blackhole/$CLUSTER";;
-  p150a) BOARD="tt_blackhole@p150a/tt_blackhole/$CLUSTER";;
-  p150b) BOARD="tt_blackhole@p150b/tt_blackhole/$CLUSTER";;
-  p150c) BOARD="tt_blackhole@p150c/tt_blackhole/$CLUSTER";;
-  p300a) BOARD="tt_blackhole@p300a/tt_blackhole/$CLUSTER";;
-  p300b) BOARD="tt_blackhole@p300b/tt_blackhole/$CLUSTER";;
-  p300c) BOARD="tt_blackhole@p300c/tt_blackhole/$CLUSTER";;
-  galaxy) BOARD="tt_blackhole@galaxy/tt_blackhole/$CLUSTER";;
-  galaxy_revc) BOARD="tt_blackhole@galaxy_revc/tt_blackhole/$CLUSTER";;
+  p100a|p150a|p150b|p150c|p300a|p300b|p300c|galaxy|galaxy_revc|orion_slt)
+  BOARD="tt_blackhole@$1/tt_blackhole/$CLUSTER";;
   *) >&2 echo "Unknown board: $1"; exit 1;;
 esac
 
