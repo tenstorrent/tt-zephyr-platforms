1cf74d73a98fb9ccf01623d3ab08684ffb806297
Aaron Fong <afong@tenstorrent.com>
2026-03-05 18:07:14 -0500

    bh_arc: throttler: adjustable tdp limit
    Add a message for setting the TDP limit. Add a new FW Table parameter to
    limit how high this new TDP limit can go. Only allow higher TDP limit on
    Galaxy for now.
    
    Signed-off-by: Aaron Fong <afong@tenstorrent.com>

--- include/tenstorrent/msgqueue.h
+++ include/tenstorrent/msgqueue.h
@@ -423,6 +423,23 @@ struct read_vm_rqst {
 	uint32_t sensor_id;
 };
 
+/** @brief Host request to set the TDP limit
+ * @details Messages of this type are processed by @ref set_tdp_limit_handler
+ */
+struct set_tdp_limit_rqst {
+	/** @brief The command code corresponding to @ref TT_SMC_MSG_SET_TDP_LIMIT */
+	uint8_t command_code;
+
+	/** @brief Three bytes of padding */
+	uint8_t pad[3];
+
+	/** @brief The TDP limit to set in watts */
+	uint32_t tdp_limit;
+
+	/** @brief Restore the TDP limit to the default value */
+	uint8_t restore_default: 1;
+};
+
 /** @brief A tenstorrent host request*/
 union request {
 	/** @brief The interpretation of the request as an array of uint32_t entries*/
@@ -492,6 +509,9 @@ union request {
 
 	/** @brief A process detector read request */
 	struct read_pd_rqst read_pd;
+
+	/** @brief A set TDP limit request */
+	struct set_tdp_limit_rqst set_tdp_limit;
 };
 
 /** @} */
