2388904aec2ceb8ca0976b148ad8f58fa8ec50a9
Daniel DeGrasse <ddegrasse@tenstorrent.com>
2026-03-24 13:54:27 -0500

    ci: write grendel smoke test logs from vdk to file
    Run grendel smoke test first, and write logs to a file
    
    Signed-off-by: Daniel DeGrasse <ddegrasse@tenstorrent.com>

--- scripts/ci/run-grendel-smoke.sh
+++ scripts/ci/run-grendel-smoke.sh
@@ -7,6 +7,25 @@
 
 set -e
 
+# Clone the VDK utilities repo, which contains a grendel fast functional SIM
+git clone git@yyz-gitlab.local.tenstorrent.com:syseng-platform/vdk-utils.git
+cd vdk-utils
+ZEPHYR_ELF=$TWISTER_DIR/tt-system-firmware/tests/drivers/tt_smc_remoteproc/
+ZEPHYR_ELF+=drivers.tt_smc_remoteproc.bl1_primary/tt_smc_remoteproc/zephyr/zephyr.elf
+PROD_ROM_ELF=../tt_smc/firmware/prod_rom-1.1.1-20260117-794e39bc/build/release/bin/prod_rom.elf
+# Watch this command until it outputs "Test PASSED"
+mkdir ../vdk-logs
+timeout 300 bash -c '
+    ./run-smc-headless.sh "$1" "$2" | while read -r line; do
+        echo "$line" > ../vdk-logs/grendel-remoteproc-smoke.log
+        if echo "$line" | grep -q "Test PASSED"; then
+            echo "Found target output, exiting..."
+            exit 0
+        fi
+    done
+' _ "$ZEPHYR_ELF" "$PROD_ROM_ELF"
+# Back out of tt-smc repo
+cd ..
 # Pull tt-smc repo to run Zephyr tests
 git clone git@yyz-gitlab.local.tenstorrent.com:tensix/tensix-hw/tt_smc.git
 cd tt_smc
@@ -25,21 +44,3 @@ cp "$TWISTER_DIR/tests/drivers/uart/"\
    ./firmware/zephyr/grendel_smc_hello_world_smp_zephyr/grendel_smc_hello_world_smp_zephyr.bin
 ttem tb_uvm/yaml/regression_smc_chiplet.yaml smc_zephyr_hello_world_smp_test \
 	--stack flist,cgen,compile_smc_chiplet,sim --no-wave --lsf --seed 1 --c compile_smc_chiplet
-# Back out of tt-smc repo
-cd ..
-# Clone the VDK utilities repo, which contains a grendel fast functional SIM
-git clone git@yyz-gitlab.local.tenstorrent.com:syseng-platform/vdk-utils.git
-cd vdk-utils
-ZEPHYR_ELF=$TWISTER_DIR/tt-system-firmware/tests/drivers/tt_smc_remoteproc/
-ZEPHYR_ELF+=drivers.tt_smc_remoteproc.bl1_primary/tt_smc_remoteproc/zephyr/zephyr.elf
-PROD_ROM_ELF=../tt_smc/firmware/prod_rom-1.1.1-20260117-794e39bc/build/release/bin/prod_rom.elf
-# Watch this command until it outputs "Test PASSED"
-timeout 300 bash -c '
-    ./run-smc-headless.sh "$1" "$2" | while read -r line; do
-        echo "$line"
-        if echo "$line" | grep -q "Test PASSED"; then
-            echo "Found target output, exiting..."
-            exit 0
-        fi
-    done
-' _ "$ZEPHYR_ELF" "$PROD_ROM_ELF"
